bd52990a9d803cf1e767d65c8b7e12f4c902988a
stdemicheli <stdemicheli@gmail.com>
2018-09-21 17:26:06 +0200

    Initial setup.

---
+++ calorie-tracker/calorie-tracker/ModelControllers/CalorieIntakeController.swift
@@ -0,0 +1,15 @@
+//
+//  CalorieIntakeController.swift
+//  calorie-tracker
+//
+//  Created by De MicheliStefano on 21.09.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+import Foundation
+
+class CalorieIntakeController {
+    
+    
+    
+}
