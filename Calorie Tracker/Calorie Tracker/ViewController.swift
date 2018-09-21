47051958f5df2119970842ffc5c258cec6d9d7a9
Samantha Gatt <smfgatt@gmail.com>
2018-09-21 11:35:55 -0400

    Adds SwiftChart dependency

---
+++ Calorie Tracker/Calorie Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calorie Tracker
+//
+//  Created by Samantha Gatt on 9/21/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
