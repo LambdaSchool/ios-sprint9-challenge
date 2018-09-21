29d11d7e41e7b386f82099b99822fc1128c95b0b
Conner Alegre <cga0@pm.me>
2018-09-21 10:47:36 -0600

    nsfetchedresultscontroller with fetching from coredata

--- ios-sprint9-challenge/ios-sprint9-challenge/View Controllers/CaloriesTableViewController.swift
+++ ios-sprint9-challenge/ios-sprint9-challenge/View Controllers/CaloriesTableViewController.swift
@@ -7,8 +7,26 @@
 //
 
 import UIKit
+import CoreData
 
-class CaloriesTableViewController: UITableViewController {
+class CaloriesTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    lazy var fetchedResultsController: NSFetchedResultsController<Calorie> = {
+        let fetchRequest: NSFetchRequest<Calorie> = Calorie.fetchRequest()
+        
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: true)]
+        
+        let moc = CoreDataManager.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                             managedObjectContext: moc,
+                                             sectionNameKeyPath: "date",
+                                             cacheName: nil)
+        frc.delegate = self
+        
+        try! frc.performFetch()
+        
+        return frc
+    }()
+    
     var calorieController = CalorieController()
     var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
@@ -16,24 +34,79 @@ class CaloriesTableViewController: UITableViewController {
         return formatter
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return calorieController.caloriesTracked.count
-    }
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CalorieTrackerCell", for: indexPath)
         
-        let amount = String(calorieController.caloriesTracked[indexPath.row].amount)
-        let date = dateFormatter.string(from: calorieController.caloriesTracked[indexPath.row].date ?? Date())
-        
+        let calorie = fetchedResultsController.object(at: indexPath)
+        let amount = String(calorie.amount)
+        let date = dateFormatter.string(from: calorie.date ?? Date())
+
         cell.textLabel?.text = "Calories: \(amount)"
         cell.detailTextLabel?.text = date
-        
+
         return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+        
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        }
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
 
     @IBAction func addCalories(_ sender: Any) {
