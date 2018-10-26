7cc09bddfb93e0d2e536263666bf59ab2f6b15d1
Moin Uddin <U_Moin75@yahoo.com>
2018-10-26 11:21:32 -0400

    Setup StoryBoard

---
+++ Calorie Tracker/Calorie Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calorie Tracker
+//
+//  Created by Moin Uddin on 10/26/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
