64a32ce837debbe6f2e1dcda62fc5022dd3f4239
iyinraphael <iyinraphael@gmail.com>
2018-10-26 09:26:53 -0700

    cocoapod set up, library created

---
+++ Calorie Tracker/Calorie Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calorie Tracker
+//
+//  Created by Iyin Raphael on 10/26/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
