6848f64a2ec5734235f3806500b65dcbce2dee79
iyinraphael <iyinraphael@gmail.com>
2018-10-26 10:20:48 -0700

    model controller created

---
+++ Calorie Tracker/Calorie Tracker/Model Controller/EntryController.swift
@@ -0,0 +1,33 @@
+//
+//  EntryController.swift
+//  Calorie Tracker
+//
+//  Created by Iyin Raphael on 10/26/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+private let moc = CoreDataStack.shared.mainContext
+
+class EntryController {
+    
+    func createCalorie( calories: Int) {
+        Entry(calories: calories)
+        save()
+        
+    }
+    
+    func save(){
+        moc.perform {
+            do {
+                try moc.save()
+            }catch{
+                NSLog("Error occured while trying to save")
+                moc.reset()
+            }
+        }
+    }
+}
+
