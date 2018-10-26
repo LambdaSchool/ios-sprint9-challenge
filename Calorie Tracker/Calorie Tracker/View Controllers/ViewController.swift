87bf1ec1890e8c5a5a15ae4d2cbf81b8e6186860
Scott Bennett <stt.btt@gmail.com>
2018-10-26 14:02:22 -0400

    Summit Project

--- Calorie Tracker/Calorie Tracker/View Controllers/ViewController.swift
+++ Calorie Tracker/Calorie Tracker/View Controllers/ViewController.swift
@@ -9,20 +9,62 @@
 import UIKit
 import SwiftChart
 
-class ViewController: UIViewController, ChartDelegate {
+class ViewController: UIViewController, ChartDelegate, UITableViewDelegate, UITableViewDataSource {
 
-    
     @IBOutlet weak var chart: Chart!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var calories: [String] = ["200", "500", "340", "1000", "270", "832", "600"]
+    let series = ChartSeries([200, 500, 340, 1000, 270, 832, 600])
+    
+    lazy var formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        return formatter
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         chart.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        
-        let series = ChartSeries([0, 6.5, 2, 8, 4.1, 7, -3.1, 10, 8])
         chart.add(series)
+    }
+    
+    
+    @IBAction func addCalorie(_ sender: Any) {
+        showInputDialog()
+
+    }
+    
+    func showInputDialog() {
+        let alertController = UIAlertController(title: "Add Calorie Intake", message: "Enter the amount of calories in field", preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "Submit", style: .default) { (_) in
+            guard let inputCalories = alertController.textFields?[0].text else { return }
+        
+            self.calories.append(inputCalories)
+            
+        }
+        
+        //the cancel action doing nothing
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+        
+        //adding textfields to our dialog box
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Calories:"
+        }
+        
+        //adding the action to dialogbox
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        //finally presenting the dialog box
+        self.present(alertController, animated: true, completion: nil)
     }
     
     func didTouchChart(_ chart: Chart, indexes: [Int?], x: Double, left: CGFloat) {
@@ -37,6 +79,23 @@ class ViewController: UIViewController, ChartDelegate {
         
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return calories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CalorieCell")
+        cell?.textLabel?.text = "Calories: \(calories[indexPath.row])"
+        cell?.detailTextLabel?.text = formatter.string(from: Date())
+        
+        return cell!
+    }
+    
+    func updateView() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
 }
 
