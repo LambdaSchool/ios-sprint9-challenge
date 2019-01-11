8922f5565e1de3bec57e6032c5df4f561e9695fb
jWarrenDev <Jerrick.Warren@gmail.com>
2019-01-11 08:16:20 -0800

    initial Commit

---
+++ CalorieTracker/CalorieTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieTracker
+//
+//  Created by Welinkton on 1/11/19.
+//  Copyright © 2019 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
