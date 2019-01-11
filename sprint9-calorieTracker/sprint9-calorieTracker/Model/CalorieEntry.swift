3abd8f0e7392afa3d038fc98e5c753aba7b6904e
OmegaWulf <nikcxz@gmail.com>
2019-01-11 11:25:18 -0500

    Model + Model Object created

---
+++ sprint9-calorieTracker/sprint9-calorieTracker/Model/CalorieEntry.swift
@@ -0,0 +1,22 @@
+//
+//  CalorieEntry.swift
+//  sprint9-calorieTracker
+//
+//  Created by Nikita Thomas on 1/11/19.
+//  Copyright © 2019 Nikita Thomas. All rights reserved.
+//
+
+import Foundation
+
+
+class CalorieEntry {
+    
+    var calories: Int
+    var date: Date
+    
+    init(calories: Int, date: Date ) {
+        self.calories = calories
+        self.date = date
+    }
+    
+}
