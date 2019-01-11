f58a776e97cfadeb20670177494897a91f4e31a3
jWarrenDev <Jerrick.Warren@gmail.com>
2019-01-11 11:00:09 -0800

    set up FRC and FRCDelegate... not working.

--- CalorieTracker/CalorieTracker/CaloriesTableViewController.swift
+++ CalorieTracker/CalorieTracker/CaloriesTableViewController.swift
@@ -10,36 +10,71 @@ import UIKit
 import CoreData
 import SwiftChart
 
-class CaloriesTableViewController: UITableViewController {
+class CaloriesTableViewController: UITableViewController, ChartDelegate, NSFetchedResultsControllerDelegate {
+  
+    // set up FRC
+    // set up FRC Delegate
+    // set up chart
+    // set up Alert
+    // ? Set up notifications
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
     
     
     // MARK: - Properties
     
     private let calorieIntakeController = CalorieIntakeController()
+    private var swiftChart: Chart = Chart(frame: .zero)
+    let reuseIdentifier = "CalorieCell"
+    var dateFormatter: DateFormatter  {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        formatter.dateStyle = .short
+        return formatter
+    }
+    
 
+    
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return fetchedResultsController.sections?.count ?? 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return fetchedResultsController.sections?.count ?? 0 // should be number of objects.. if I remember.
+        
     }
-
     
+
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+     
+     let calorieIntake = fetchedResultsController.object(at: indexPath)
+        
+        cell.textLabel?.text = "Caloric Intake: \(calorieIntake.calories)"
+        cell.detailTextLabel?.text = dateFormatter.string(for: calorieIntake.date)
+     
+     return cell
+     }
+    
+
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+    
+    
+    // UIAlert Controller
     @IBAction func addCalories(_ sender: Any) {
         
         // um... Alert Controller thing?
@@ -66,55 +101,70 @@ class CaloriesTableViewController: UITableViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    // MARK: - ChartViewOutlet
+    @IBOutlet weak var swiftChartView: UIView!
+    
+    
+    // MARK: FRCDelegate
+    
+    /*
+     Assume self has a property 'tableView' -- as is the case for an instance of a UITableViewController
+     subclass -- and a method configureCell:atIndexPath: which updates the contents of a given cell
+     with information from a managed object at the given index path in the fetched results controller.
+     */
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        
+    }
+   
+    // MARK: FRC
+    
+        var fetchedResultsController: NSFetchedResultsController<CalorieIntake> = {
+        let fetchRequest: NSFetchRequest<CalorieIntake> = CalorieIntake.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        let moc = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                             managedObjectContext: moc,
+                                             sectionNameKeyPath: nil,
+                                             cacheName: nil)
+        frc.delegate = self
+        try! frc.performFetch()
+        return frc
+    }()
     
     
     
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+     
+     
+     // MARK:  Chart Delegate - ?? Do I need this..
+     
+     func didTouchChart(_ chart: Chart, indexes: [Int?], x: Double, left: CGFloat) {
+     
+     }
+     
+     func didFinishTouchingChart(_ chart: Chart) {
+    
+     }
+     
+     func didEndTouchingChart(_ chart: Chart) {
+    
+     }
+     
+     
+     
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -122,6 +172,6 @@ class CaloriesTableViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
