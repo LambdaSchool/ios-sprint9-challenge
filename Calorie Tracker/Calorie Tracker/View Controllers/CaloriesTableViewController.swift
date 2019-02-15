4713ac23e4adc9e5fb501d0b1f0978d383f28996
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-02-15 10:39:05 -0600

    Alert Controller added for user-entered data

--- Calorie Tracker/Calorie Tracker/View Controllers/CaloriesTableViewController.swift
+++ Calorie Tracker/Calorie Tracker/View Controllers/CaloriesTableViewController.swift
@@ -6,8 +6,46 @@ class CaloriesTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+        showInputPopUp()
+        
+    }
+    
+    func showInputPopUp() {
+        
+        // Create UIAlertController, set title and message
+        let alertController = UIAlertController(title: "Add Calorie Intake", message: "Enter the amount of calories in the field below", preferredStyle: .alert)
+        
+        // Confirm action taking the inputs
+        let confirmAction = UIAlertAction(title: "Submit", style: .default) { (_) in
+            
+            // Get the input values from user
+            let calories = alertController.textFields?[0].text
+            
+            // Put the user-entered amount into the caloriesInput property
+            // caloriesInput = calories(calories: calories, timestamp: Date())
+        }
+        
+        // Cancel action does nothing
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+        
+        // Add textfield to popup
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Calories:"
+        }
+        
+        // Add action to popup
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        // Present the popup
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
     
 }
