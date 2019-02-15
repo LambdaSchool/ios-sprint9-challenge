f08bb0a3938dc0dff34581a14867b1e57294dd07
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-02-15 12:53:41 -0600

    Notifications set up for chart to update when notification posts about new data

--- Calorie Tracker/Calorie Tracker/View Controllers/CaloriesTableViewController.swift
+++ Calorie Tracker/Calorie Tracker/View Controllers/CaloriesTableViewController.swift
@@ -2,14 +2,35 @@
 import Foundation
 import SwiftChart
 
+extension NSNotification.Name {
+    static let shouldShowChartDataChanged = NSNotification.Name("ShouldShowChartDataChanged")
+}
+
 class CaloriesTableViewController: UITableViewController {
+    
+    private var chart: Chart?
+    
+    @IBOutlet weak var chartView: Chart!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        chartSetUp()
+        
+        // Register for notifications
+        NotificationCenter.default.addObserver(self, selector: #selector(shouldShowChartDataChanged(_:)), name: .shouldShowChartDataChanged, object: nil)
     }
     
+    // MARK: - Notifications
     
+    @objc func shouldShowChartDataChanged(_ notification: Notification) {
+        
+        chartSetUp()
+        tableView.reloadData()
+    }
+    
+    // MARK: - Alert Controller
+
     @IBAction func addButton(_ sender: Any) {
         showInputPopUp()
     }
@@ -29,8 +50,13 @@ class CaloriesTableViewController: UITableViewController {
             //guard let caloriesInt = caloriesInt else { return }
             
             // Put the user-entered amount into the caloriesInput property
-            // caloriesInput = calories(calories: calories, timestamp: Date())
             self.caloriesInput.append(CalorieInput(calories: caloriesInt!))
+            
+            // Put the integers only in to an array that will populate the chart
+            self.calorieIntsForChart.append(Double(caloriesInt!))
+            
+            // Post a notification
+            NotificationCenter.default.post(name: .shouldShowChartDataChanged, object: self)
         }
         
         // Cancel action does nothing
@@ -50,10 +76,14 @@ class CaloriesTableViewController: UITableViewController {
         
     }
     
+    // MARK: UITableViewDataSource
+    
+    // Number of rows
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return caloriesInput.count
     }
     
+    // Cell contents
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
@@ -66,11 +96,33 @@ class CaloriesTableViewController: UITableViewController {
         return cell
     }
     
+    // MARK: - Chart
+    
+    func chartSetUp() {
+        
+        guard let chart = chart else { return }
+        
+        //chart = Chart(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+        let series = ChartSeries(calorieIntsForChart)
+        chart.add(series)
+
+        series.color = ChartColors.cyanColor()
+        chart.add(series)
+    }
+    
+    
+    // MARK: - Properties
+    
     var caloriesInput: [CalorieInput] = [] {
         didSet {
             tableView.reloadData()
         }
     }
+    
+    var calorieInput: CalorieInput!
+    
+    // test data
+    var calorieIntsForChart: [Double] = [150.0, 200.0]
     
 
 }
---
+++ Calorie Tracker/Calorie Tracker/View Controllers/Chart.swift
@@ -0,0 +1,22 @@
+
+//import UIKit
+//import SwiftChart
+//
+//class Chart: Chart {
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        let chart = Chart(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+//        let series = ChartSeries(calorieIntsForChart)
+//        chart.add(series)
+//        
+//        series.color = ChartColors.cyanColor()
+//        chart.add(series)
+//        
+//    }
+//
+//    lazy var caloriesTableViewController = CaloriesTableViewController()
+//    lazy var calorieIntsForChart = caloriesTableViewController.calorieIntsForChart
+//
+//}
