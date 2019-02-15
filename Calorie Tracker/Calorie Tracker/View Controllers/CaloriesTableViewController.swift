cdd88c99ebbcfc9213310ef3dab9de20eb228411
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-02-15 10:22:46 -0600

    Added SwiftChart pod and set up user interface

---
+++ Calorie Tracker/Calorie Tracker/View Controllers/CaloriesTableViewController.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+import SwiftChart
+
+class CaloriesTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
