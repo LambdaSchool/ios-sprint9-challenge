764f16b51207d73eef293bc166ea7cc03fadbbc9
Moses Robinson <mosesjrobinson@icloud.com>
2019-03-08 10:33:14 -0800

    fetched results controller an delegate set up

--- Calorie Tracker/Calorie Tracker/View Controllers/CalorieChartTableViewController.swift
+++ Calorie Tracker/Calorie Tracker/View Controllers/CalorieChartTableViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import SwiftChart
+import CoreData
 
-class CalorieChartTableViewController: UITableViewController {
+class CalorieChartTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,13 +21,10 @@ class CalorieChartTableViewController: UITableViewController {
         
     }
     
-    
-    
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
-        return 0
+        return fetchedResultsController.fetchedObjects?.count ?? 0
     }
 
     let reuseIdentifier = "CalorieIntakeCell"
@@ -34,7 +32,9 @@ class CalorieChartTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
 
-        // Configure the cell...
+        let calorieIntake = fetchedResultsController.object(at: indexPath)
+        
+        cell.textLabel?.text = "Calories: \(calorieIntake.calories)"
 
         return cell
     }
@@ -42,14 +42,78 @@ class CalorieChartTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             
+            let calorieIntake = fetchedResultsController.object(at: indexPath)
             
+            calorieIntakeController.delete(calorieIntake: calorieIntake)
+        }
+    }
+    
+    // MARK: - NSFetchedResultsControllerDelegate
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
             
-            tableView.deleteRows(at: [indexPath], with: .fade)
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let indexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.moveRow(at: indexPath, to: newIndexPath)
+            
+            //            tableView.deleteRows(at: [indexPath], with: .automatic)
+            //            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        
+        switch type {
+            
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
         }
     }
 
-
     // MARK: - Properties
+    
+    var calorieIntakeController = CalorieIntakeController()
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<CalorieIntake> = {
+        
+        let fetchRequest: NSFetchRequest<CalorieIntake> = CalorieIntake.fetchRequest()
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(key: "timestamp", ascending: false)
+        ]
+        
+        let moc = CoreDataStack.shared.mainContext
+        
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
+        
+        frc.delegate = self
+        try! frc.performFetch()
+        
+        return frc
+    }()
     
     @IBOutlet var calorieChart: Chart!
 }
