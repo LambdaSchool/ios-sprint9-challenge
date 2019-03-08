ec1079a061deacdf17d6e2408f2f0b3ce8e7d574
Moses Robinson <mosesjrobinson@icloud.com>
2019-03-08 10:18:07 -0800

    application now has a calorieintake controller

---
+++ Calorie Tracker/Calorie Tracker/Model Controller/CalorieIntakeController.swift
@@ -0,0 +1,35 @@
+//
+//  CalorieIntakeController.swift
+//  Calorie Tracker
+//
+//  Created by Moses Robinson on 3/8/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+import Foundation
+
+class CalorieIntakeController {
+    
+    func saveToPersistentStore() {
+        do {
+            try CoreDataStack.shared.save()
+        } catch {
+            CoreDataStack.shared.mainContext.reset()
+            NSLog("Error saving managed object context: \(error)")
+        }
+    }
+    
+    func create(calories: Int16) {
+        
+        _ = CalorieIntake(calories: calories)
+        saveToPersistentStore()
+    }
+    
+    func delete(calorieIntake: CalorieIntake) {
+        
+        let moc = CoreDataStack.shared.mainContext
+        moc.delete(calorieIntake)
+        saveToPersistentStore()
+    }
+    
+}
--- Calorie Tracker/Calorie Tracker/Model/CoreDataStack.swift
+++ Calorie Tracker/Calorie Tracker/Model/CoreDataStack.swift
@@ -13,6 +13,21 @@ class CoreDataStack {
     
     static let shared = CoreDataStack()
     
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        
+        var error: Error?
+        
+        context.performAndWait {
+            
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+    
     lazy var container: NSPersistentContainer = {
         
         let appName = Bundle.main.object(forInfoDictionaryKey: (kCFBundleNameKey as String)) as! String
@@ -24,6 +39,7 @@ class CoreDataStack {
                 fatalError("Failed to load persistent stores: \(error)")
             }
         }
+        container.viewContext.automaticallyMergesChangesFromParent = true
         return container
     }()
     
