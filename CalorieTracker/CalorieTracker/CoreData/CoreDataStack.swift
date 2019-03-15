cc22489f007be5caac1c3df0b7e6454e8f33d0e9
Frulwinn <frulwinn@gmail.com>
2019-03-15 08:45:12 -0700

    added xcdatamodeld and core data stack

---
+++ CalorieTracker/CalorieTracker/CoreData/CoreDataStack.swift
@@ -0,0 +1,33 @@
+//
+//  CoreDataStack.swift
+//  CalorieTracker
+//
+//  Created by Lambda_School_Loaner_34 on 3/15/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        
+        // Give the container the name of your data model file
+        let container = NSPersistentContainer(name: "CalorieTracker")
+        
+        // Load the persistent store
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
