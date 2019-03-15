2281e6d68a547663d105540932cf8896802a261e
nateyoungren <nathanaelyoungren@gmail.com>
2019-03-15 08:42:45 -0700

    Build entry controller.

---
+++ CalorieTracker/CalorieTracker/EntryController.swift
@@ -0,0 +1,48 @@
+//
+//  EntryController.swift
+//  CalorieTracker
+//
+//  Created by Nathanael Youngren on 3/15/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class EntryController {
+    
+    var entries: [Entry] {
+        return loadFromCoreDataStore()
+    }
+    
+    func createEntry(numberOfCalories: Int16) {
+        _ = Entry(calories: numberOfCalories)
+        saveToCoreDataStore()
+    }
+    
+    func loadFromCoreDataStore() -> [Entry] {
+        
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: false)]
+        
+        let moc = CoreDataStack.shared.mainContext
+        
+        do {
+            return try moc.fetch(fetchRequest)
+        } catch {
+            NSLog("Error fetching from core data.")
+            return []
+        }
+    }
+    
+    func saveToCoreDataStore(moc: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        moc.perform {
+            do {
+                try moc.save()
+            } catch {
+                moc.reset()
+                NSLog("Error saving to core data.")
+            }
+        }
+    }
+}
