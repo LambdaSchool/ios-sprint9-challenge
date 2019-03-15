2d2ea7c2c69fd3ac1725fb297a176454454c5852
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-03-15 11:27:29 -0400

    create a core data stack

---
+++ CalorieTracker/CalorieTracker/Core Data/CoreDataStack.swift
@@ -0,0 +1,46 @@
+//
+//  CoreDataStack.swift
+//  CalorieTracker
+//
+//  Created by Julian A. Fordyce on 3/15/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        
+        let appName = Bundle.main.object(forInfoDictionaryKey: (kCFBundleNameKey as String)) as! String
+        let container = NSPersistentContainer(name: appName)
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var error: Error?
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+}
