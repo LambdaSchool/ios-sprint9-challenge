85dfae9981b71859e122188d78ba3cdb62e0b15d
nateyoungren <nathanaelyoungren@gmail.com>
2019-03-15 08:24:29 -0700

    Install framework and setup storyboard

---
+++ CalorieTracker/CalorieTracker/CalorieTrackerTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalorieTrackerTableViewController.swift
+//  CalorieTracker
+//
+//  Created by Nathanael Youngren on 3/15/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class CalorieTrackerTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath)
+
+        return cell
+    }
+
+}
---
+++ CalorieTracker/CalorieTracker/ChartViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChartViewController.swift
+//  CalorieTracker
+//
+//  Created by Nathanael Youngren on 3/15/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class ChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
