79a366e5fb007598388eadc431fb96301da6e8eb
Frulwinn <frulwinn@gmail.com>
2019-03-15 10:08:35 -0700

    NSFetched Restuls Controller Delegate

--- CalorieTracker/CalorieTracker/Controllers/CalorieTableViewController.swift
+++ CalorieTracker/CalorieTracker/Controllers/CalorieTableViewController.swift
@@ -15,11 +15,16 @@ class CalorieTableViewController: UITableViewController {
     //MARK: - Properties
     var calorieController = CalorieController()
     var series: ChartSeries!
+    
+    //MARK: - Outlets
+    @IBAction func addCalories(_ sender: Any) {
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
     }
 
     // MARK: - Table view data source
@@ -37,6 +42,46 @@ class CalorieTableViewController: UITableViewController {
 
         
         return cell
+    }
+    
+    // MARK: - NSFetchedResultsControllerDelegate
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let indexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.moveRow(at: indexPath, to: newIndexPath)
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
     }
 
 }
