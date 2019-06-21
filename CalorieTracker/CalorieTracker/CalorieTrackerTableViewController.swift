5d58413a920bd71728ca5598575165d09a7207d2
victor <ruizdvictor@gmail.com>
2019-06-21 12:20:32 -0400

    updated storyboard logic

---
+++ CalorieTracker/CalorieTracker/CalorieTrackerTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CalorieTrackerTableViewController.swift
+//  CalorieTracker
+//
+//  Created by Victor  on 6/21/19.
+//  Copyright © 2019 Victor . All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Charts
+
+class CalorieTrackerTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
