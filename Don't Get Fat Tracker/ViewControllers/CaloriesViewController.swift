179303fd646751c40eb27f69b41d2e47cb4f9f79
Michael Redig <mredig@gmail.com>
2019-06-21 10:56:34 -0500

    added CoreData basics reorganized

--- Don't Get Fat Tracker/ViewControllers/CaloriesViewController.swift
+++ Don't Get Fat Tracker/ViewControllers/CaloriesViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 import SwiftChart
+import CoreData
 
 class CaloriesViewController: UIViewController {
+
+	lazy var fetchedResultsController: NSFetchedResultsController<Calories> = {
+		let fetchRequest: NSFetchRequest<Calories> = Calories.fetchRequest()
+		fetchRequest.sortDescriptors = [NSSortDescriptor(key: "person", ascending: true), NSSortDescriptor(key: "timestamp", ascending: false)]
+
+		let moc = CoreDataStack.shared.mainContext
+		let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+																  managedObjectContext: moc,
+																  sectionNameKeyPath: "person",
+																  cacheName: nil)
+		fetchedResultsController.delegate = self
+		do {
+			try fetchedResultsController.performFetch()
+		} catch {
+			print("error performing initial fetch for frc: \(error)")
+		}
+		return fetchedResultsController
+	}()
+
+	@IBOutlet var tableView: UITableView!
 	@IBOutlet var chartParentView: UIView!
 	let chart = Chart()
 
@@ -65,6 +86,51 @@ extension CaloriesViewController: UITableViewDelegate, UITableViewDataSource {
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		return tableView.dequeueReusableCell(withIdentifier: "CalorieCell", for: indexPath)
 	}
-
-
 }
+
+// MARK: - Fetched Results Controller Delegate
+extension CaloriesViewController: NSFetchedResultsControllerDelegate {
+	func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+		tableView.beginUpdates()
+	}
+
+	func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+		tableView.endUpdates()
+	}
+
+	func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+		let indexSet = IndexSet([sectionIndex])
+		switch type {
+		case .insert:
+			tableView.insertSections(indexSet, with: .automatic)
+		case .delete:
+			tableView.deleteSections(indexSet, with: .automatic)
+		default:
+			print(#line, #file, "unexpected NSFetchedResultsChangeType: \(type)")
+		}
+	}
+
+	func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+		switch type {
+		case .insert:
+			guard let newIndexPath = newIndexPath else { return }
+			tableView.insertRows(at: [newIndexPath], with: .automatic)
+		case .move:
+			guard let newIndexPath = newIndexPath, let indexPath = indexPath else { return }
+			tableView.moveRow(at: indexPath, to: newIndexPath)
+		case .update:
+			guard let indexPath = indexPath else { return }
+			tableView.reloadRows(at: [indexPath], with: .automatic)
+		case .delete:
+			guard let indexPath = indexPath else { return }
+			tableView.deleteRows(at: [indexPath], with: .automatic)
+		@unknown default:
+			print(#line, #file, "unknown NSFetchedResultsChangeType: \(type)")
+		}
+	}
+
+	func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, sectionIndexTitleForSectionName sectionName: String) -> String? {
+		return nil
+	}
+}
+
