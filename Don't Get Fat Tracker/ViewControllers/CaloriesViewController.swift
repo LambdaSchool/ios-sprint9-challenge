baea80b239fa75afd3e801fa85399d3a86b235e7
Michael Redig <mredig@gmail.com>
2019-06-21 10:46:53 -0500

    added chart function

--- Don't Get Fat Tracker/ViewControllers/CaloriesViewController.swift
+++ Don't Get Fat Tracker/ViewControllers/CaloriesViewController.swift
@@ -11,6 +11,7 @@ import SwiftChart
 
 class CaloriesViewController: UIViewController {
 	@IBOutlet var chartParentView: UIView!
+	let chart = Chart()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -18,9 +19,9 @@ class CaloriesViewController: UIViewController {
 	}
 
 	func setupChart() {
-		let chart = Chart(frame: CGRect.zero)
 		chartParentView.addSubview(chart)
 
+		chart.minY = 0
 		chart.translatesAutoresizingMaskIntoConstraints = false
 		chart.topAnchor.constraint(equalTo: chartParentView.topAnchor).isActive = true
 		chart.bottomAnchor.constraint(equalTo: chartParentView.bottomAnchor).isActive = true
@@ -29,6 +30,30 @@ class CaloriesViewController: UIViewController {
 	}
 
 	@IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+		let alert = UIAlertController(title: "Add Calorie Intake", message: "Careful what you eat!", preferredStyle: .alert)
+		var calorieTextField: UITextField?
+		alert.addTextField { (textField) in
+			calorieTextField = textField
+			calorieTextField?.addTarget(self, action: #selector(self.calorieTextFieldEdited), for: .editingChanged)
+		}
+		let action = UIAlertAction(title: "Add", style: .default) { [weak self] (action) in
+			guard let calorieString = calorieTextField?.text, let calorieAmount = Double(calorieString) else { return }
+			if let series = self?.chart.series.first {
+				series.data.append((x: Double(series.data.count), y: calorieAmount))
+				self?.chart.add(series)
+		} else {
+				let series = ChartSeries([calorieAmount])
+				self?.chart.add(series)
+			}
+		}
+		let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+		alert.addAction(action)
+		alert.addAction(cancel)
+		present(alert, animated: true)
+	}
+
+	@IBAction func calorieTextFieldEdited(_ sender: UITextField) {
+		sender.text = sender.text?.replacingOccurrences(of: ##"\D"##, with: "", options: .regularExpression, range: nil)
 	}
 }
 
