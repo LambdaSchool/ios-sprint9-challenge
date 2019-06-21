93e769343048435a52ff0141e9ee608017f05dc7
victor <ruizdvictor@gmail.com>
2019-06-21 12:37:03 -0400

    added alert action functionality

--- CalorieTracker/CalorieTracker/CalorieTrackerTableViewController.swift
+++ CalorieTracker/CalorieTracker/CalorieTrackerTableViewController.swift
@@ -12,8 +12,26 @@ import Charts
 
 class CalorieTrackerTableViewController: UITableViewController {
     
+    //MARK: Properties
+    
+    @IBOutlet weak var addCalorieIntakeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func addCalorieIntakeButtonPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Add Calorie Intake", message: "Enter the amount of calories in the field", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Calories:"
+        }
+        alert.addAction(UIAlertAction(title: "Submit", style: .default, handler: { [weak alert] (_) in
+            guard let textField = alert?.textFields![0] else {return}
+            print(textField.text ?? "No Intake")
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
