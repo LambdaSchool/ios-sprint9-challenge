977db8ac77e106f9d326086f55e1797de0562342
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-28 08:25:52 -0700

    tableView Setup

---
+++ calorie-tracker/calorie-tracker/Viewcontrollers/CaloriesTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CaloriesTableViewController.swift
+//  calorie-tracker
+//
+//  Created by Hector Steven on 6/28/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+import SwiftChart
+
+class CaloriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+	}
+
+}
+
+extension CaloriesTableViewController: ChartDelegate {
+	func didTouchChart(_ chart: Chart, indexes: [Int?], x: Double, left: CGFloat) {
+		
+	}
+	
+	func didFinishTouchingChart(_ chart: Chart) {
+		
+	}
+	
+	func didEndTouchingChart(_ chart: Chart) {
+		
+	}
+	
+	
+}
