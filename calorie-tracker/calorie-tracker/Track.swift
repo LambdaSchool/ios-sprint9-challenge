ee6ee2a024638fa59c9b4edccbbbe84ed21bcae9
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-28 08:47:43 -0700

    track

---
+++ calorie-tracker/calorie-tracker/Track.swift
@@ -0,0 +1,15 @@
+//
+//  Track.swift
+//  calorie-tracker
+//
+//  Created by Hector Steven on 6/28/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
+
+struct Track {
+	//let id: String
+	let caloriesCount: Int
+	let date: String
+}
