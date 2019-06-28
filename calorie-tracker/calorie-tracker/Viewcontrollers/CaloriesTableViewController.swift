bc8a2b4a5e8aeb1748485c6bc507c95af31cfb78
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-28 08:44:06 -0700

    tableview setup

--- calorie-tracker/calorie-tracker/Viewcontrollers/CaloriesTableViewController.swift
+++ calorie-tracker/calorie-tracker/Viewcontrollers/CaloriesTableViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 import SwiftChart
 
 class CaloriesTableViewController: UITableViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-		
+		chart.delegate = self
 	}
 
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 10
+	}
+	
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: "CalorieTableViewCell", for: indexPath) as?
+			CalorieTableViewCell else { return UITableViewCell()}
+		
+		cell.calorieLabel?.text = "\(indexPath.row)"
+		return cell
+	}
+	
+	@IBOutlet var chart: Chart!
 }
 
 extension CaloriesTableViewController: ChartDelegate {
