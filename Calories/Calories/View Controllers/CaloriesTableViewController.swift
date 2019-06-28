36b627291ffde9924b29e527f8603c9eb50aacc8
Hayden Hastings <haydenhastings32@gmail.com>
2019-06-28 10:31:29 -0600

    Added new file CaloriesTableViewController and cleaned up unnecessary code

---
+++ Calories/Calories/View Controllers/CaloriesTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CaloriesTableViewController.swift
+//  Calories
+//
+//  Created by Hayden Hastings on 6/28/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import UIKit
+
+class CaloriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+}
