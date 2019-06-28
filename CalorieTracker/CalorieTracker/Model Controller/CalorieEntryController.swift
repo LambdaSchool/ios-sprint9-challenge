cc271c2b3e64333d7028e819e822a6c220dbed05
Mitchell Budge <mitchellbudge@gmail.com>
2019-06-28 09:16:04 -0600

    Configure Main.storyboard

---
+++ CalorieTracker/CalorieTracker/Model Controller/CalorieEntryController.swift
@@ -0,0 +1,9 @@
+//
+//  CalorieEntryController.swift
+//  CalorieTracker
+//
+//  Created by Mitchell Budge on 6/28/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
