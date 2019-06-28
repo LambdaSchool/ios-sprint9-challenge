22a0be4eaf117465bbf232f42fcd83e71aee3389
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-28 08:33:31 -0700

    CalorieTableViewCell

---
+++ calorie-tracker/calorie-tracker/Viewcontrollers/CalorieTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CalorieTableViewCell.swift
+//  calorie-tracker
+//
+//  Created by Hector Steven on 6/28/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class CalorieTableViewCell: UITableViewCell {
+
+	
+	
+	
+	@IBOutlet var calorieLabel: UILabel!
+	@IBOutlet var dateLabel: UILabel!
+}
