4adfb7d0234ff86946db7f70c253186e332b84e4
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-06-28 11:12:06 -0400

    First commit ... installed pod file ...

---
+++ CalorieSprint/CalorieSprint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieSprint
+//
+//  Created by Ryan Murphy on 6/28/19.
+//  Copyright © 2019 Ryan Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
