f249d0f3f7f64eb6dcc57648d5dbdfb08931ee17
John Pitts <jgpitts@gmail.com>
2019-07-05 13:11:43 -0400

    added CoreDataStack with a save method which will take load off main thread when saving years worth of daily calorie data.  used 'maincontext' shortcut property making 'container' lazily created. shares is stack constant

---
+++ CalorieTrackerSPRINT/CalorieTrackerSPRINT/CoreDataStack.swift
@@ -0,0 +1,45 @@
+//
+//  CoreDataStack.swift
+//  CalorieTrackerSPRINT
+//
+//  Created by John Pitts on 7/5/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.maincontext) throws {
+        
+        var error: Error?
+        
+        context.performAndWait {   // opens a background thread to lower load on main thread, so save is efficient
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CalorieTrackerSPRINT")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores \(error)")
+                
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var maincontext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
