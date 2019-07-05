69155688f1cf977fe9a41498a3c6ee7d52743484
Michael R.F <mrflowers22@uwalumni.com>
2019-07-05 08:49:11 -0700

    implemented the alert window functionality

--- CalorieTracker/CalorieTracker/View Controller/CalorieTrackerViewController.swift
+++ CalorieTracker/CalorieTracker/View Controller/CalorieTrackerViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class CalorieTrackerViewController: UIViewController {
 
+    @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func popUpToAddCalories(){
+        let alert = UIAlertController(title: "Add Calorie Intake.", message: "Enter the amount of calories in the text field", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        var myTextField: UITextField!
+        alert.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "Add Calories"
+            myTextField = textField
+        })
+        
+        let okAction = UIAlertAction(title: "Submit", style: .default) { (_) in
+            //TODO: ADD WHAT'S IN THE TEXT TO THE TABLEVIEW
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    @IBAction func addCalories(_ sender: UIBarButtonItem) {
+        popUpToAddCalories()
     }
 }
