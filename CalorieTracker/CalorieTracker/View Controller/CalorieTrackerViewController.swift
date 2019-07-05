c298209c8a7d09da962eec2fb963ced72ed71b03
Michael R.F <mrflowers22@uwalumni.com>
2019-07-05 08:35:53 -0700

    made calorieTracker file

---
+++ CalorieTracker/CalorieTracker/View Controller/CalorieTrackerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CalorieTrackerViewController.swift
+//  CalorieTracker
+//
+//  Created by Michael Flowers on 7/5/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class CalorieTrackerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
