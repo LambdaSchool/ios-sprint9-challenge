5e189b18eec18d94d07e79d5bad39574e7314edc
Kat Milton <mltnkat@gmail.com>
2019-08-23 16:05:57 -0700

    cleanup whitespace.

--- CalorieTracker/CalorieTracker/Model Controller/CalorieCountController.swift
+++ CalorieTracker/CalorieTracker/Model Controller/CalorieCountController.swift
@@ -11,8 +11,6 @@ import CoreData
 
 class CalorieCountController {
     
-   
-    
     func createEntry(with intakeAmount: Double) {
         let calorieCount = CalorieCount(intakeNumber: intakeAmount)
         let moc = CoreDataStack.shared.mainContext
@@ -22,63 +20,7 @@ class CalorieCountController {
             } catch {
                 NSLog("Error saving entry to context: \(error)")
             }
-        
-      
     }
-    
-//    func deleteEntry(calorieCount: CalorieCount) {
-//        let moc = CoreDataStack.shared.mainContext
-//        moc.delete(calorieCount)
-//        saveToCoreDataStack(nsLogMessage: "deleteEntry: Error saving context: ")
-//    }
-    
-//    private func updateEntries(with representations: [CalorieCountRepresentation], context: NSManagedObjectContext) {
-//
-//        context.performAndWait {
-//
-//            for representation in representations {
-//                guard let date = representation.date,
-//                    let intakeNumber = representation.intakeNumber else { return }
-//
-//                if let calorieCount = entry(for: date, context: context) {
-//                    calorieCount.date = date
-//                    calorieCount.intakeNumber = intakeNumber
-//                } else {
-//                  CalorieCount(calorieCountRepresentation: representation, context: context)
-//                }
-//            }
-//        }
-//    }
-    
-//    func fetchEntries(context: NSManagedObjectContext) -> [CalorieCount]? {
-//        let fetchRequest: NSFetchRequest<CalorieCount> = CalorieCount.fetchRequest()
-////        let predicate = NSPredicate(format: "date == %@", date as NSDate)
-////        fetchRequest.predicate = predicate
-////
-//        var result: [CalorieCount]? = []
-//
-//        context.performAndWait {
-//            do {
-//                let calorieCount = try context.fetch(fetchRequest)
-//                result = calorieCount
-//            } catch {
-//                NSLog("Error fetching calorieCount: \(error)")
-//            }
-//        }
-//        return result
-//    }
-//
-//
-//
-//    func saveToCoreDataStack(nsLogMessage: String) {
-//
-//        do {
-//            try CoreDataStack.shared.save()
-//        } catch {
-//            NSLog(nsLogMessage, "\(error)")
-//        }
-//    }
-    
 }
 
 extension NSNotification.Name {
