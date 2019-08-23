ec80d8d17e078cbc3aeb3dea5b5492b1e1083ccb
Kat Milton <mltnkat@gmail.com>
2019-08-23 09:16:33 -0700

    update the model controller.

--- CalorieTracker/CalorieTracker/Model/CalorieCountController.swift
+++ CalorieTracker/CalorieTracker/Model/CalorieCountController.swift
@@ -7,8 +7,65 @@
 //
 
 import Foundation
+import CoreData
 
 class CalorieCountController {
     
+    func createEntry(with intakeAmount: String, date: Date? = Date()) -> CalorieCount {
+        let calorieCount = CalorieCount(intakeNumber: intakeAmount, date: date)
+        saveToCoreDataStack(nsLogMessage: "createEntry: Error saving context: ")
+        return calorieCount
+    }
+    
+    func deleteEntry(calorieCount: CalorieCount) {
+        let moc = CoreDataStack.shared.mainContext
+        moc.delete(calorieCount)
+        saveToCoreDataStack(nsLogMessage: "deleteEntry: Error saving context: ")
+    }
+    
+    private func updateEntries(with representations: [CalorieCountRepresentation], context: NSManagedObjectContext) {
+        
+        context.performAndWait {
+            
+            for representation in representations {
+                guard let date = representation.date else { return }
+                
+                if let calorieCount = entry(for: date, context: context) {
+                    calorieCount.date = representation.date
+                    calorieCount.intakeNumber = representation.intakeNumber
+                } else {
+                  CalorieCount(calorieCountRepresentation: representation, context: context)
+                }
+            }
+        }
+    }
+    
+    private func entry(for date: Date, context: NSManagedObjectContext) -> CalorieCount? {
+        let fetchRequest: NSFetchRequest<CalorieCount> = CalorieCount.fetchRequest()
+        let predicate = NSPredicate(format: "date == %@", date as NSDate)
+        fetchRequest.predicate = predicate
+        
+        var result: CalorieCount? = nil
+        
+        context.performAndWait {
+            do {
+                let calorieCount = try context.fetch(fetchRequest).first
+                result = calorieCount
+            } catch {
+                NSLog("Error fetching calorieCount from date: \(date.timeIntervalSince1970): \(error)")
+            }
+        }
+        return result
+    }
+    
+    
+    
+    func saveToCoreDataStack(nsLogMessage: String) {
+        do {
+            try CoreDataStack.shared.save()
+        } catch {
+            NSLog(nsLogMessage, "\(error)")
+        }
+    }
     
 }
