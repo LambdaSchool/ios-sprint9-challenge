1e30b3c546c6632ea2303df2f828edb4ffc2b0c3
seanacres <6416690+seanacres@users.noreply.github.com>
2019-08-23 11:25:31 -0400

    Add core data stack and calorie controller

---
+++ CalorieTracker/CalorieTracker/Controllers/CalorieController.swift
@@ -0,0 +1,13 @@
+//
+//  CalorieController.swift
+//  CalorieTracker
+//
+//  Created by Sean Acres on 8/23/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import Foundation
+
+class CalorieController {
+    
+}
