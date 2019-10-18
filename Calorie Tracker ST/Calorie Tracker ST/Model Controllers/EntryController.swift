ec8c428d1f2d060ce386023f1d88e7e2a7a8252b
Jake <jlconnerly@gmail.com>
2019-10-18 12:03:06 -0500

    EntryController CRUD methods complete

--- Calorie Tracker ST/Calorie Tracker ST/Model Controllers/EntryController.swift
+++ Calorie Tracker ST/Calorie Tracker ST/Model Controllers/EntryController.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+import CoreData
+
+class entryController {
+    
+    var entries: [Entry] = []
+    
+    func createEntry(calorieCount: Int, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        context.performAndWait {
+            let convertedInt = Int32(calorieCount)
+            let newEntry = Entry(calories: convertedInt, dateEntered: Date())
+            entries.append(newEntry)
+            do {
+                try CoreDataStack.shared.save(context: context)
+                NotificationCenter.default.post(name: .entriesUpdated, object: self)
+            } catch {
+                NSLog("Error saving context when creating new Entry:\(error)")
+            }
+        }
+    }
+    
+    func updateEntry(entry: Entry, calorieCount: Int, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        context.performAndWait {
+            let convertedInt = Int32(calorieCount)
+            entry.calories = convertedInt
+            entry.dateEntered = Date()
+            
+            do {
+                try CoreDataStack.shared.save(context: context)
+                NotificationCenter.default.post(name: .entriesUpdated, object: self)
+            } catch {
+                NSLog("Error saving context when updating Entry:\(error)")
+            }
+        }
+    }
+    
+    func deleteEntry(entry: Entry, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        context.performAndWait {
+            context.delete(entry)
+            do {
+                try CoreDataStack.shared.save(context: context)
+                NotificationCenter.default.post(name: .entriesUpdated, object: self)
+            } catch {
+                NSLog("Error saving context when deleting Entry:\(error)")
+            }
+        }
+    }
+    
+    
+}
