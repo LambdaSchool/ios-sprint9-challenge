4325ae41bb45f9810574d0038a7aec93988ec86e
Jake <jlconnerly@gmail.com>
2019-10-18 11:44:06 -0500

    Added entryController and notification files

---
+++ Calorie Tracker ST/Calorie Tracker ST/Model Controllers/EntryController.swift
@@ -0,0 +1,9 @@
+//
+//  EntryController.swift
+//  Calorie Tracker ST
+//
+//  Created by Jake Connerly on 10/18/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import Foundation
