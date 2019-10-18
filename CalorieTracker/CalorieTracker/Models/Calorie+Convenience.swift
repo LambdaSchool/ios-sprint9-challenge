25e9d791d3a1820f7c7015b3b58d80282fd04918
ladybeitel <ciarabeitel@gmail.com>
2019-10-18 11:59:53 -0400

    Create CoreData Model and Stack. Clean up code using SwiftLint.

---
+++ CalorieTracker/CalorieTracker/Models/Calorie+Convenience.swift
@@ -0,0 +1,22 @@
+//
+//  Calorie+Convenience.swift
+//  CalorieTracker
+//
+//  Created by Ciara Beitel on 10/18/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Calorie {
+    
+    convenience init(identifier: UUID = UUID(), intake: Int16, timestamp: Date, context: NSManagedObjectContext) {
+        
+        self.init(context: context)
+        
+        self.identifier = identifier
+        self.intake = intake
+        self.timestamp = timestamp
+    }
+}
