c481ebdbcb8376f8aa163879b5ef352db8b494e8
Jake <jlconnerly@gmail.com>
2019-10-18 13:48:00 -0500

    Changed chart color lintAutoCorrect

--- Calorie Tracker ST/Calorie Tracker ST/View Controllers/CalorieChartViewController.swift
+++ Calorie Tracker ST/Calorie Tracker ST/View Controllers/CalorieChartViewController.swift
@@ -38,6 +38,7 @@ class CalorieChartViewController: UIViewController {
     func setUpChart() {
         
         let series = ChartSeries([0, 6.5, 2, 8, 4.1, 7, -3.1, 10, 8])
+        series.color = ChartColors.darkGreenColor()
         series.area = true
         chartView.add(series)
     }
