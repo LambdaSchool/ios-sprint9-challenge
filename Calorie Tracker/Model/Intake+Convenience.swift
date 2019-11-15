702eed9522ff24fa0cc5a3552ab3a96826e203dc
Wilma <kima_92@fastmail.com>
2019-11-15 14:32:41 -0500

    Added Calorie Intake Model

---
+++ Calorie Tracker/Model/Intake+Convenience.swift
@@ -0,0 +1,25 @@
+//
+//  Intake+Convenience.swift
+//  Calorie Tracker
+//
+//  Created by macbook on 11/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Intake {
+    
+    // This initializer sets up the Core Data (NSManagedObject) part of the Intake, then gives it the properties unique to an Intake entity.
+    
+    @discardableResult convenience init(calories: Int16, date: Date = Date(), id: UUID = UUID(), context: NSManagedObjectContext) {
+        
+        // Calling the designated initializer
+        self.init(context: context)
+        
+        self.calories = calories
+        self.date = date
+        self.id = id
+    }
+}
