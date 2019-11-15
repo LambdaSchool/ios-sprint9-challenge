0bf4a4a17cc9a1dd97c9ed574b6eafe0c67dae2b
Wilma <kima_92@fastmail.com>
2019-11-15 10:58:07 -0500

    Initial Commit

---
+++ Calorie Tracker/Calorie Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calorie Tracker
+//
+//  Created by macbook on 11/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
