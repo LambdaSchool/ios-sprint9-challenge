362d82e66362088e74d8b49a80eb4ac2f5bfe695
Jerry Haaser <jerry.haaser@hotmail.com>
2019-11-15 12:14:34 -0600

    set up model and model controller, created UI, set up addEntryVIewCOntroller and CalorieTrackerTableVIewController

---
+++ CalorieTracker/CalorieTracker/Helper/CoreDataStack.swift
@@ -0,0 +1,52 @@
+//
+//  CoreDataStack.swift
+//  CalorieTracker
+//
+//  Created by Jerry haaser on 11/15/19.
+//  Copyright © 2019 Jerry haaser. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    private init() { }
+    
+    // lazy doesn't initialize upone class initialization, it is only called when needed
+    lazy var container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Entry")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error {
+                fatalError("Core sata was unable to load persistence stores: \(error)")
+            }
+        })
+        
+        return container
+    }()
+    
+    var backgroundContext: NSManagedObjectContext {
+        return container.newBackgroundContext()
+    }
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func save() {
+        
+        let context = CoreDataStack.shared.mainContext
+        
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch {
+                NSLog("error saving context: \(error)")
+                context.reset()
+            }
+        }
+    }
+}
---
+++ CalorieTracker/CalorieTracker/ViewControllers/AddEntryViewController.swift
@@ -0,0 +1,49 @@
+//
+//  AddEntryViewController.swift
+//  CalorieTracker
+//
+//  Created by Jerry haaser on 11/15/19.
+//  Copyright © 2019 Jerry haaser. All rights reserved.
+//
+
+import UIKit
+
+class AddEntryViewController: UIViewController {
+    
+    var entryController: EntryController?
+    
+    @IBOutlet weak var caloriesTextField: UITextField!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        
+        guard let calories = caloriesTextField.text,
+            !calories.isEmpty else { return }
+        
+        entryController?.createEntry(with: Double(calories)!)
+        NotificationCenter.default.post(name: "chartSeriesChanged", object: self)
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
