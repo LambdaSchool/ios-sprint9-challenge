047268bc3a5fa8ceb77484df7280784ebd9681b8
Joel Groomer <joelgroomer@gmail.com>
2019-12-14 09:50:42 -0800

    Storyboard laid out.

---
+++ CalorieCounter/CalorieCounter/Views/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  CalorieCounter
+//
+//  Created by Joel Groomer on 12/14/19.
+//  Copyright © 2019 Julltron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
