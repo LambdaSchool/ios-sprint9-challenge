1ae6eb083f62bd1725c61c672aa09bb1bc258a31
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-20 12:03:45 -0700

    View Controller refactored to use NSFetchedResultsController to populate data; Add SwiftLint

--- CalorieTrackerSprintChallenge/CalorieTrackerSprintChallenge/View Controllers/ViewController.swift
+++ CalorieTrackerSprintChallenge/CalorieTrackerSprintChallenge/View Controllers/ViewController.swift
@@ -8,19 +8,42 @@
 
 import UIKit
 import SwiftChart
+import CoreData
 
 class ViewController: UIViewController {
-    
+    // MARK: - IBOutlets and Properties
     var entryController = CalorieEntryController()
     let df = DateFormatter()
     var series: ChartSeries?
+    lazy var frc: NSFetchedResultsController<CalorieEntry> = {
+        
+        let fetchRequest: NSFetchRequest<CalorieEntry> = CalorieEntry.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: true)]
+        
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                             managedObjectContext: CoreDataStack.shared.mainContext,
+                                             sectionNameKeyPath: "timestamp",
+                                             cacheName: nil)
+        
+        frc.delegate = self.calorieEntryTableView as? NSFetchedResultsControllerDelegate
+        
+        do {
+            try frc.performFetch()
+        } catch {
+            fatalError("Error performing fetch for frc: \(error)")
+        }
+        
+        return frc
+    }()
     
     @IBOutlet weak var calorieChart: Chart!
     @IBOutlet weak var calorieEntryTableView: UITableView!
     
+    // MARK: - IBActions and Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         calorieEntryTableView.delegate = self
+        calorieEntryTableView.dataSource = self
         NotificationCenter.default.addObserver(self, selector: #selector(updateViews), name: .newEntry, object: nil)
     }
     
@@ -34,9 +57,7 @@ class ViewController: UIViewController {
         let action = UIAlertAction(title: "Save", style: .default) { (_) in
             guard let calorieAmount = alert.textFields?.first?.text,
                 !calorieAmount.isEmpty else { return }
-            let newEntry = CalorieEntryRep(amount: calorieAmount, timestamp: Date())
             self.entryController.createEntry(amount: calorieAmount, timestamp: Date())
-            self.entryController.entriesArray.append(newEntry)
             self.updateViews()
         }
         
@@ -51,22 +72,25 @@ class ViewController: UIViewController {
     
 }
 
+// MARK: - ViewController Extensions
 extension ViewController: UITableViewDelegate {
     
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return entryController.entriesArray.count
+        return frc.sections?[section].numberOfObjects ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = calorieEntryTableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath)
+        let entry = frc.object(at: indexPath)
+        guard let timestamp = entry.timestamp else { return UITableViewCell() }
         df.dateStyle = .medium
         df.timeStyle = .short
         
-        cell.textLabel?.text = entryController.entriesArray[indexPath.row].amount
-        cell.detailTextLabel?.text = df.string(from: entryController.entriesArray[indexPath.row].timestamp)
+        cell.textLabel?.text = entry.amount
+        cell.detailTextLabel?.text = df.string(from: timestamp)
         
         return cell
     }
