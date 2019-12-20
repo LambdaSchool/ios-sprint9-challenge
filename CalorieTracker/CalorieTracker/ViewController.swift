97013831251a95f9e01a3184ed5500a18d8392c7
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-12-20 08:13:00 -0800

    Added Calorie Tracker Project

---
+++ CalorieTracker/CalorieTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieTracker
+//
+//  Created by Lambda_School_Loaner_204 on 12/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
