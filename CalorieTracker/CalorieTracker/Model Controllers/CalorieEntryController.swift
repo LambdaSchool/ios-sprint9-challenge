ec8e8d92f7f22b0f1ebcea33be1baba552137500
Percy <percival.ngan@gmail.com>
2019-12-20 12:34:49 -0600

    Added to tableview controller

---
+++ CalorieTracker/CalorieTracker/Model Controllers/CalorieEntryController.swift
@@ -0,0 +1,9 @@
+//
+//  CalorieEntryController.swift
+//  CalorieTracker
+//
+//  Created by Percy Ngan on 12/20/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
