00368855fbe0db7d0189f058305a994b91c68962
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-20 11:12:22 -0700

    Most everything done, just need tableView and chart to populate; also need to add SwiftLint

--- CalorieTrackerSprintChallenge/CalorieTrackerSprintChallenge/View Controllers/ViewController.swift
+++ CalorieTrackerSprintChallenge/CalorieTrackerSprintChallenge/View Controllers/ViewController.swift
@@ -11,25 +11,42 @@ import SwiftChart
 
 class ViewController: UIViewController {
     
+    var entryController = CalorieEntryController()
+    let df = DateFormatter()
+    var series: ChartSeries?
+    
     @IBOutlet weak var calorieChart: Chart!
     @IBOutlet weak var calorieEntryTableView: UITableView!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calorieEntryTableView.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(updateViews), name: .newEntry, object: nil)
     }
     
     @IBAction func addCalorieEntryTapped(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add Entry", message: "", preferredStyle: .alert)
         alert.addTextField { (textField) in
+            textField.keyboardType = .numberPad
             textField.placeholder = "Enter Calorie Amount"
         }
         // TODO: - finish action after EntryController has been created
         let action = UIAlertAction(title: "Save", style: .default) { (_) in
-            <#code#>
+            guard let calorieAmount = alert.textFields?.first?.text,
+                !calorieAmount.isEmpty else { return }
+            let newEntry = CalorieEntryRep(amount: calorieAmount, timestamp: Date())
+            self.entryController.createEntry(amount: calorieAmount, timestamp: Date())
+            self.entryController.entriesArray.append(newEntry)
+            self.updateViews()
         }
+        
+        alert.addAction(action)
+        self.present(alert, animated: true)
+    }
+    
+    @objc func updateViews() {
+        calorieEntryTableView.reloadData()
+        calorieChart.reloadInputViews()
     }
     
 }
@@ -40,11 +57,18 @@ extension ViewController: UITableViewDelegate {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return entryController.entriesArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = calorieEntryTableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath)
+        df.dateStyle = .medium
+        df.timeStyle = .short
+        
+        cell.textLabel?.text = entryController.entriesArray[indexPath.row].amount
+        cell.detailTextLabel?.text = df.string(from: entryController.entriesArray[indexPath.row].timestamp)
+        
+        return cell
     }
     
     
