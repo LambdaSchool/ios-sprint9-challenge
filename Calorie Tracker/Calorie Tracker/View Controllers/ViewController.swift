19032276de08d76c9336681515ae211e75190f2f
Rwoltx <richardx1016@gmail.com>
2019-12-20 15:14:12 -0500

    1st

---
+++ Calorie Tracker/Calorie Tracker/View Controllers/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Calorie Tracker
+//
+//  Created by Rick Wolter on 12/20/19.
+//  Copyright © 2019 Devshop7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
