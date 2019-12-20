de6186ac5f93f2dc9d5ce748aa71c6622c297e6f
Jerry Haaser <jerry.haaser@hotmail.com>
2019-12-20 11:43:04 -0600

    set up model and installed SwiftChart

---
+++ CalorieTrackerApp/CalorieTrackerApp/Model/Entry+Convenience.swift
@@ -0,0 +1,18 @@
+//
+//  Entry+Convenience.swift
+//  CalorieTrackerApp
+//
+//  Created by Jerry haaser on 12/20/19.
+//  Copyright © 2019 Jerry haaser. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    convenience init(calories: Double, date: Date = Date(), context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context = context)
+        self.date = date
+        self.calories = calories
+    }
+}
---
+++ CalorieTrackerApp/CalorieTrackerApp/Model/EntryController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryController.swift
+//  CalorieTrackerApp
+//
+//  Created by Jerry haaser on 12/20/19.
+//  Copyright © 2019 Jerry haaser. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import SwiftChart
+
+class EntryController {
+    func createEntry(with calories: Double) {
+        let entry = Entry(calories: calories)
+        CoreDataStack.shared.save()
+    }
+    
+    func delete(entry: Entry) {
+        let context = CoreDataStack.shared.mainContext
+        context.performAndWait {
+            context.delete(entry)
+            CoreDataStack.shared.save()
+        }
+    }
+    
+    func update(entry: Entry, calories: Double) {
+        
+    }
+}
