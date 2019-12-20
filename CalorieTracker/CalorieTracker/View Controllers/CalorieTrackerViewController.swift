0c0b80703731b77fa26159d3dcc6f4ae993738f9
Jon Bash <jonbash@protonmail.com>
2019-12-20 10:32:29 -0800

    set up view controller

--- CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerViewController.swift
+++ CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerViewController.swift
@@ -15,6 +15,58 @@ class CalorieTrackerViewController: UIViewController {
     @IBOutlet private weak var calorieChartView: Chart!
     @IBOutlet private weak var entryTableView: UITableView!
 
-    @IBOutlet private weak var addEntryButtonTapped: UIBarButtonItem!
+    private lazy var calorieEntryController = CalorieEntryController()
 
+    // MARK: - Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        calorieEntryController.delegate = self
+        setUpChart()
+        entryTableView.reloadData()
+    }
+
+    @IBAction func addEntryButtonTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(
+            title: "Add calorie intake",
+            message: "Enter the amount of calories in the field.",
+            preferredStyle: .alert)
+
+        alert.addTextField { textField in
+            textField.placeholder = "# of calories"
+        }
+
+        alert.addAction(UIAlertAction(
+            title: "Cancel",
+            style: .cancel,
+            handler: nil))
+        alert.addAction(UIAlertAction(
+            title: "Add entry",
+            style: .default,
+            handler: { [unowned alert] _ in
+                guard
+                    let caloriesText = alert.textFields?[0].text,
+                    let calories = Double(caloriesText)
+                    else { return }
+
+                _ = CalorieEntry(calories: calories)
+
+                do {
+                    try self.calorieEntryController.saveToPersistentStore()
+                } catch {
+                    NSLog("Error saving entry to persistent store: \(error)")
+                }
+        }))
+
+        present(alert, animated: true, completion: nil)
+    }
+
+    func setUpChart() {
+        var data = [Double]()
+        if let entries = calorieEntryController.fetchedResultsController.fetchedObjects {
+            data = entries.map { $0.calories }
+        }
+        calorieChartView.add(ChartSeries(data))
+    }
 }
