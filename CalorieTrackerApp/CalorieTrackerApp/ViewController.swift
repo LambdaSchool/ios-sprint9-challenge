7b459ecd66e866bd526dd97b0a2014d46e20bc69
Jerry Haaser <jerry.haaser@hotmail.com>
2019-12-20 11:11:45 -0600

    set up UI

---
+++ CalorieTrackerApp/CalorieTrackerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieTrackerApp
+//
+//  Created by Jerry haaser on 12/20/19.
+//  Copyright © 2019 Jerry haaser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
