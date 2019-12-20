631bd1c118960046b77ad8322803f1c49ac460af
Dan M <djmthedev@gmail.com>
2019-12-20 08:59:22 -0800

    added CalorieIntake+Convenience

---
+++ SprintChallengeCalorieTracker/SprintChallengeCalorieTracker/Models/CalorieIntake+Convenience.swift
@@ -0,0 +1,11 @@
+//
+//  CalorieIntake+Convenience.swift
+//  SprintChallengeCalorieTracker
+//
+//  Created by morse on 12/20/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+
