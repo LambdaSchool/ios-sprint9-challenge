bbcf899da2b7e03a31400556cc83bf13aa034c6f
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-20 09:52:00 -0700

    Create UI, add UIAlertController in ViewController.swift for adding caloric entries

--- CalorieTrackerSprintChallenge/CalorieTrackerSprintChallenge/ViewController.swift
+++ CalorieTrackerSprintChallenge/CalorieTrackerSprintChallenge/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import SwiftChart
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var calorieChart: Chart!
+    @IBOutlet weak var calorieEntryTableView: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func addCalorieEntryTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add Entry", message: "", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Enter Calorie Amount"
+        }
+        // TODO: - finish action after EntryController has been created
+        let action = UIAlertAction(title: "Save", style: .default) { (_) in
+            <#code#>
+        }
+    }
+    
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
