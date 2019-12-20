ca3e7a990ceadd16ba70f6b6e5dc65637cdac6eb
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-20 16:11:20 -0700

    Commit for new branch start

--- CalorieTrackerSprintChallenge/CalorieTrackerSprintChallenge/View Controllers/ViewController.swift
+++ CalorieTrackerSprintChallenge/CalorieTrackerSprintChallenge/View Controllers/ViewController.swift
@@ -13,6 +13,7 @@ import CoreData
 class ViewController: UIViewController {
     // MARK: - IBOutlets and Properties
     var entryController = CalorieEntryController()
+    var entriesArray: [CalorieEntryRep] = []
     let df = DateFormatter()
     var series: ChartSeries?
     lazy var frc: NSFetchedResultsController<CalorieEntry> = {
@@ -25,7 +26,7 @@ class ViewController: UIViewController {
                                              sectionNameKeyPath: "timestamp",
                                              cacheName: nil)
         
-        frc.delegate = self.calorieEntryTableView as? NSFetchedResultsControllerDelegate
+        frc.delegate = self
         
         do {
             try frc.performFetch()
@@ -45,29 +46,35 @@ class ViewController: UIViewController {
         calorieEntryTableView.delegate = self
         calorieEntryTableView.dataSource = self
         NotificationCenter.default.addObserver(self, selector: #selector(updateViews), name: .newEntry, object: nil)
+        if let series = series {
+            calorieChart.add(series)
+        }
     }
     
     @IBAction func addCalorieEntryTapped(_ sender: UIBarButtonItem) {
+        
         let alert = UIAlertController(title: "Add Entry", message: "", preferredStyle: .alert)
         alert.addTextField { (textField) in
             textField.keyboardType = .numberPad
             textField.placeholder = "Enter Calorie Amount"
         }
-        // TODO: - finish action after EntryController has been created
+        
         let action = UIAlertAction(title: "Save", style: .default) { (_) in
             guard let calorieAmount = alert.textFields?.first?.text,
                 !calorieAmount.isEmpty else { return }
             self.entryController.createEntry(amount: calorieAmount, timestamp: Date())
-            self.updateViews()
+//            self.updateViews()
         }
         
         alert.addAction(action)
         self.present(alert, animated: true)
     }
     
+    
     @objc func updateViews() {
-        calorieEntryTableView.reloadData()
+        series = ChartSeries(entriesArray.compactMap({ Double($0.amount) }))
         calorieChart.reloadInputViews()
+        calorieEntryTableView.reloadData()
     }
     
 }
@@ -78,8 +85,12 @@ extension ViewController: UITableViewDelegate {
 }
 
 extension ViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return frc.sections?[section].numberOfObjects ?? 0
+        guard let objects = frc.sections?[section].numberOfObjects else { return 0 }
+//        print(frc.sections?[section].numberOfObjects)
+        print("Passed first print")
+        return objects
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -94,6 +105,55 @@ extension ViewController: UITableViewDataSource {
         
         return cell
     }
-    
-    
+}
+
+extension ViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        calorieEntryTableView.beginUpdates()
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        calorieEntryTableView.endUpdates()
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            calorieEntryTableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            calorieEntryTableView.deleteRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let indexPath = indexPath, let newIndexPath = newIndexPath else { return }
+            calorieEntryTableView.moveRow(at: indexPath, to: newIndexPath)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            calorieEntryTableView.reloadRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            fatalError()
+        }
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+
+        let indexSet = IndexSet(integer: sectionIndex)
+
+        switch type {
+        case .insert:
+            calorieEntryTableView.insertSections(indexSet, with: .automatic)
+        case .delete:
+            calorieEntryTableView.deleteSections(indexSet, with: .automatic)
+        default:
+            return
+        }
+    }
 }
