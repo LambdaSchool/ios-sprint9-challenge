448ea1b821f91be528605e6c155df3e4b75eeb84
Thomas Sabino-Benowitz <sabinowitz@gmail.com>
2020-01-31 14:12:19 -0500

    Commit1

---
+++ calorie tracker/calorie tracker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  calorie tracker
+//
+//  Created by Thomas Sabino-Benowitz on 12/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
