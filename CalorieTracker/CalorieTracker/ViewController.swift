af7bb3faeb204bc0b45d2cc20a1fb65d5b09aeca
Craig Swanson <cswanson02@me.com>
2020-02-23 20:18:36 -0600

    Created model and CoreDataStack

---
+++ CalorieTracker/CalorieTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieTracker
+//
+//  Created by Craig Swanson on 2/23/20.
+//  Copyright © 2020 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
