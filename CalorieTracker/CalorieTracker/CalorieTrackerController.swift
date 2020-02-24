569017402ffffd2e5bcaea9468cd36f7091cb205
Craig Swanson <cswanson02@me.com>
2020-02-23 21:39:33 -0600

    Added table view and chart to the view controller

---
+++ CalorieTracker/CalorieTracker/CalorieTrackerController.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTrackerController.swift
+//  CalorieTracker
+//
+//  Created by Craig Swanson on 2/23/20.
+//  Copyright © 2020 Craig Swanson. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import SwiftChart
+
+class CalorieTrackerController {
+    
+    var entries: [CalorieTracker] = []
+    
+}
---
+++ CalorieTracker/CalorieTracker/CalorieTrackerViewController.swift
@@ -0,0 +1,48 @@
+//
+//  CalorieTrackerViewController.swift
+//  CalorieTracker
+//
+//  Created by Craig Swanson on 2/23/20.
+//  Copyright © 2020 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class CalorieTrackerViewController: UIViewController {
+    
+    var calorieTrackerController = CalorieTrackerController()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// MARK: Table View Data Source
+extension CalorieTrackerViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        calorieTrackerController.entries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
