cf5f4356677dad8ba1c6cbcaebc27fd6c25120e9
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-02-28 08:07:46 -0800

    Initial Commit

---
+++ CodeQualitySprint/Controller/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeQualitySprint
+//
+//  Created by Kenny on 2/28/20.
+//  Copyright © 2020 Hazy Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
