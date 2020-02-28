6d82917bda43871a6fb6b7c68cff6dc522ac2928
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-02-28 09:34:01 -0800

    added coredata in tableview

--- CalorieTrackerSprint/CalorieTrackerSprint/ChartTableViewController.swift
+++ CalorieTrackerSprint/CalorieTrackerSprint/ChartTableViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import CoreData
 import SwiftChart
 
 class ChartTableViewController: UITableViewController {
+    
+    // Bad way of doing this
+    var calorieIntakeArray: [CalorieIntake] {
+        let fetchRequest: NSFetchRequest<CalorieIntake> = CalorieIntake.fetchRequest()
+        let context = CoreDataStack.shared.mainContext
+        do {
+            return try context.fetch(fetchRequest)
+        } catch {
+            NSLog("Error fetching calorieIntakes: \(error)")
+            return []
+        }
+    }
     
     fileprivate lazy var alertController: UIAlertController = {
         let ac = UIAlertController(title: "Add Calorie Intake", message: "Enter the amount of calories below", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Submit", style: .default, handler: { (action) in
             print(self.tf?.text ?? "")
+            self.saveCalorieIntake()
+            self.tableView.reloadData()
         }))
         
         ac.addTextField { (textField) in
@@ -23,16 +38,41 @@ class ChartTableViewController: UITableViewController {
         return ac
     }()
     
+    var dateFormatter: DateFormatter = {
+       let formatter = DateFormatter()
+       formatter.dateStyle = .short
+       formatter.timeStyle = .short
+       return formatter
+    }()
+    
     fileprivate var tf: UITextField?
     
     @IBAction func addButton(_ sender: UIBarButtonItem) {
         present(alertController, animated: true)
     }
     
+    func saveCalorieIntake() {
+        guard let calories = tf!.text, !calories.isEmpty else {return}
+        
+        let cals: Int = Int(calories) ?? 0 // why does this need to be unwrapped again?
+        let _ = CalorieIntake(calories: cals, dateEntered: Date())
+        // Save
+        do {
+            try CoreDataStack.shared.mainContext.save()
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
 
 
@@ -45,18 +85,21 @@ class ChartTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return calorieIntakeArray.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        // Configure the cell...
+        let calorieIntake = calorieIntakeArray[indexPath.row]
+        cell.textLabel?.text = "Calories: \(calorieIntake.calories)"
+        cell.detailTextLabel?.text = dateFormatter.string(from: calorieIntake.dateEntered ?? Date())
 
         return cell
     }
     
+
 
     /*
     // Override to support conditional editing of the table view.
