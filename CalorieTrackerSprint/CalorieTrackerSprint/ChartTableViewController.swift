0dca2425187de562a3e8289d78fc4c0813f756f9
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-02-28 10:19:43 -0800

    uses notifications now

--- CalorieTrackerSprint/CalorieTrackerSprint/ChartTableViewController.swift
+++ CalorieTrackerSprint/CalorieTrackerSprint/ChartTableViewController.swift
@@ -12,9 +12,10 @@ import SwiftChart
 
 class ChartTableViewController: UITableViewController {
     
+    // MARK: - Properties
+    
     @IBOutlet weak var chart: Chart!
     
-    // Bad way of doing this
     var calorieIntakeArray: [CalorieIntake] {
         let fetchRequest: NSFetchRequest<CalorieIntake> = CalorieIntake.fetchRequest()
         let context = CoreDataStack.shared.mainContext
@@ -39,8 +40,8 @@ class ChartTableViewController: UITableViewController {
         ac.addAction(UIAlertAction(title: "Submit", style: .default, handler: { (action) in
             print(self.tf?.text ?? "")
             self.saveCalorieIntake()
-            self.refreshChart()
-            self.tableView.reloadData()
+            // ring bell
+            NotificationCenter.default.post(name: .updateViews, object: self)
         }))
         
         ac.addTextField { (textField) in
@@ -77,10 +78,8 @@ class ChartTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // fill in
-        calorieSeriesArray.area = true
+        NotificationCenter.default.addObserver(self, selector: #selector(refreshChart), name: .updateViews, object: nil)
         chart.add(calorieSeriesArray)
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -88,9 +87,12 @@ class ChartTableViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    func refreshChart() {
+    /// Called when notification is heard
+    @objc func refreshChart() {
+        print("heard bell ring")
         chart.removeAllSeries()
         chart.add(calorieSeriesArray)
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
@@ -104,7 +106,6 @@ class ChartTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return calorieIntakeArray.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
@@ -115,52 +116,4 @@ class ChartTableViewController: UITableViewController {
 
         return cell
     }
-    
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
