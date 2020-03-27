42de15692ae3f3ac430ab60707d85cc00f403450
Ufuk Türközü <ufuk_trkz@hotmail.de>
2020-03-27 20:24:18 +0100

    MVP

--- Calorie Tracker/Calorie Tracker/CaloriesTableViewController.swift
+++ Calorie Tracker/Calorie Tracker/CaloriesTableViewController.swift
@@ -37,18 +37,10 @@ class CaloriesTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        
+        updateChart()
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(updateChart),
+                                               name: .updateChart, object: nil)
     }
     
     // MARK: - Table view data source
@@ -63,77 +55,105 @@ class CaloriesTableViewController: UITableViewController {
         return fetchResultsController.sections?[section].numberOfObjects ?? 0
     }
     
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "CalorieCell", for: indexPath)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CalorieCell", for: indexPath)
         
-     let entry = fetchResultsController.object(at: indexPath)
-     // Configure the cell...
-        cell.textLabel?.text = "\(entry.calories)"
-        cell.detailTextLabel?.text = "\(String(describing: entry.date))"
-     
-     return cell
-     }
+        let entry = fetchResultsController.object(at: indexPath)
+        // Configure the cell...
+        cell.textLabel?.text = "\(entry.calories) kcal"
+        cell.detailTextLabel?.text = dateFormatter.string(from: entry.date ?? Date())
+        
+        return cell
+    }
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    @IBAction func add(_ sender: Any) {
-        let alert = UIAlertController(title: "Add Calories", message: nil, preferredStyle: .alert)
-        alert.addTextField { textField in
-        textField.placeholder = "Enter Calories:"
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            if editingStyle == .delete {
+                let entry = self.fetchResultsController.object(at: indexPath)
+                self.calorieController.delete(entry: entry)
+                self.tableView.reloadData()
+                NotificationCenter.default.post(name: .updateChart, object: self)
+            }
         }
-        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { action in
+    }
+    
+    @IBAction func add(_ sender: Any) {
+        let alert = UIAlertController(title: "Again?", message: "You said you would eat less 🙄", preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "kcal:"
+        }
+        alert.addAction(UIAlertAction(title: "Let me live my life", style: .default, handler: { action in
             if let calories = alert.textFields?.first?.text, !calories.isEmpty {
                 self.calorieController.create(with: Int16(calories) ?? 0, date: Date())
+                NotificationCenter.default.post(name: .updateChart, object: self)
             }
         }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         self.present(alert, animated: true)
+    }
+    
+    let dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.timeStyle = .short
+        df.dateStyle = .short
+        return df
+    }()
+    
+    @objc func updateChart() {
+        let calories = fetchResultsController.fetchedObjects ?? []
+        var calorieArr: [Double] = [0]
+        
+        for calorie in calories {
+            calorieArr.append(Double(calorie.calories))
+        }
+        
+        chartView.removeAllSeries()
+        
+        let series = ChartSeries(calorieArr)
+        series.color = ChartColors.cyanColor()
+        series.area = true
+        
+        chartView.add(series)
     }
     
 }
 
 extension CaloriesTableViewController: NSFetchedResultsControllerDelegate {
-    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            break
+        }
+    }
 }
