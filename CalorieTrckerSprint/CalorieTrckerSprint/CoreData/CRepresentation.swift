d26db8384e32c0a48092c849bd91080bfaf8d5ab
elizabeth paige <wingateep@Icloud.com>
2020-03-27 11:22:43 -0400

    added project + TVC + Calorie CoreData

---
+++ CalorieTrckerSprint/CalorieTrckerSprint/CoreData/CRepresentation.swift
@@ -0,0 +1,14 @@
+//
+//  CRepresentation.swift
+//  CalorieTrckerSprint
+//
+//  Created by Elizabeth Wingate on 3/27/20.
+//  Copyright © 2020 Elizabeth Wingate. All rights reserved.
+//
+
+import Foundation
+
+struct CalorieRepresentation: Codable {
+    let calorie: Int16
+    let timestamp: Date
+}
