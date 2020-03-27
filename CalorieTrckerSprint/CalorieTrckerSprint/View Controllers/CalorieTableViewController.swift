f5ca9837e49d0ab9e14eb0f69fdadf726fcfef87
elizabeth paige <wingateep@Icloud.com>
2020-03-27 12:07:52 -0400

    added cocoapod + cleaned up alittle

--- CalorieTrckerSprint/CalorieTrckerSprint/View Controllers/CalorieTableViewController.swift
+++ CalorieTrckerSprint/CalorieTrckerSprint/View Controllers/CalorieTableViewController.swift
@@ -13,7 +13,7 @@ class CalorieTableViewController: UITableViewController {
     let calorieController = CalorieController()
 
     lazy var fetchedResultController: NSFetchedResultsController<Calorie> = {
-        // fetch request
+  
         let fetchRequest: NSFetchRequest<Calorie> = Calorie.fetchRequest()
         fetchRequest.sortDescriptors = [
             NSSortDescriptor(key: "calorie", ascending: true),
@@ -38,7 +38,6 @@ class CalorieTableViewController: UITableViewController {
      }()
 
      func dateString(for calorie: Calorie?) -> String? {
-         // execute with map if there is any date
          return calorie?.timestamp.map { dateFormatter.string(from: $0) }
      }
 
@@ -84,53 +83,6 @@ class CalorieTableViewController: UITableViewController {
         
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension CalorieTableViewController: NSFetchedResultsControllerDelegate {
@@ -174,4 +126,3 @@ extension CalorieTableViewController: NSFetchedResultsControllerDelegate {
         }
     }
 }
-
