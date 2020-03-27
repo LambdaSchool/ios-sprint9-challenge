cf42be65dc0b09745dc778798ca93051b98b5e27
Chris <chris.gonzales.82@gmail.com>
2020-03-27 13:51:22 -0700

    Chart implemented

--- Calorie Tracker/Calorie Tracker/Helpers/Alerts.swift
+++ Calorie Tracker/Calorie Tracker/Helpers/Alerts.swift
@@ -11,22 +11,5 @@ import UIKit
 
 struct Alerts {
     
-    func callAddEntry() -> String? {
-        let alert = UIAlertController(title: "Add Calorie Intake",
-                                      message: "Enter the amount of calories in the field.", preferredStyle: .actionSheet)
-        alert.addTextField { (textfield) in
-            textfield.placeholder = "Input caloric intake here"
-        }
-        let textFields = alert.textFields
-        let alertTextField = textFields![0]
-        
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        alert.addAction(UIAlertAction(title: "Submit", style: .default))
-        
-        alert.present(alert, animated: true)
-        
-        guard let inputCalories = alertTextField.text else { return nil }
-        
-        return inputCalories
-    }
+    
 }
--- Calorie Tracker/Calorie Tracker/View Controllers/CalorieEntryTableViewController.swift
+++ Calorie Tracker/Calorie Tracker/View Controllers/CalorieEntryTableViewController.swift
@@ -7,13 +7,13 @@
 //
 
 import UIKit
+import SwiftChart
 
 class CalorieEntryTableViewController: UITableViewController {
     
     // MARK: - Properties:
     
-    var calorieController: CalorieController?
-    
+    var calorieController = CalorieController()
     
     // MARK: - View Lifecycle
     
@@ -21,11 +21,20 @@ class CalorieEntryTableViewController: UITableViewController {
         super.viewDidLoad()
     }
     
+    // MARK: - Actions
+    
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+        addEntryAlert()
+    }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var chart: Chart!
+    
     // MARK: - Private Methods
     
-    private func setupCell(for cell: UITableViewCell, at indexPath: IndexPath) {
-        guard let calorieController = calorieController,
-            let date = calorieController.calorieEntries[indexPath.row].date else { return }
+    private func setupCell(for cell: UITableViewCell, at indexPath: IndexPath) -> UITableViewCell {
+        guard let date = calorieController.calorieEntries[indexPath.row].date else { return UITableViewCell()}
         let entry = calorieController.calorieEntries[indexPath.row]
         
         let calorieCount = Int(entry.count)
@@ -35,20 +44,58 @@ class CalorieEntryTableViewController: UITableViewController {
         
         cell.textLabel?.text = titleString
         cell.detailTextLabel?.text = dateString
+        
+        return cell
+    }
+    private func updateChart() {
+        var chartEntries: [Double] = []
+        
+        for entry in calorieController.calorieEntries {
+            let double = Double(entry.count)
+            chartEntries.append(double)
+        }
+        
+        let series = ChartSeries(chartEntries)
+        chart.add(series)
+    }
+    private func addEntryAlert() {
+        
+        let alert = UIAlertController(title: "Add Calorie Intake",
+                                      message: "Enter the amount of calories in the field.", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: {
+            (textfield) in
+            textfield.placeholder = "Input caloric intake here"
+        })
+        let textFields = alert.textFields
+        let alertTextField = textFields![0]
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Submit",
+                                      style: .default,
+                                      handler: { action in
+                                        guard let inputCalories = alertTextField.text,
+                                            !inputCalories.isEmpty,
+                                            let caloriesInt = Int(inputCalories) else { return }
+                                        self.calorieController.createEnry(withCalorieCount: caloriesInt)
+                                        self.updateChart()
+                                        self.tableView.reloadData()
+        }))
+        
+        present(alert, animated: true)
+        
+        return
     }
     
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return calorieController?.calorieEntries.count ?? 0
+        return calorieController.calorieEntries.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CalorieCell", for: indexPath)
         
-        setupCell(for: cell, at: indexPath)
-        
-        return cell
+        return setupCell(for: cell, at: indexPath)
     }
     
 }
