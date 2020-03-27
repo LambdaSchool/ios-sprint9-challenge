6b75b31296b23fe7bae30f45a62e29597eb8f100
Chris <chris.gonzales.82@gmail.com>
2020-03-27 09:30:17 -0700

    Added subviews to Main.storyboard.  Created CalorieTrackerViewController, and CalorieEntryTableViewController files

---
+++ Calorie Tracker/Calorie Tracker/View Controllers/CalorieEntryTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CalorieEntryTableViewController.swift
+//  Calorie Tracker
+//
+//  Created by Chris Gonzales on 3/27/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import UIKit
+
+class CalorieEntryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+}
