20eb7d3074ea7af80c4bf8b773bb6ce35f67eac4
Wyatt Harrell <wyattaharrell@gmail.com>
2020-04-24 11:20:02 -0400

    Setup outlets and extension for uitableviewdelegate and datasource

--- Calorie Tracker/Calorie Tracker/View Controllers/CalorieTrackerViewController.swift
+++ Calorie Tracker/Calorie Tracker/View Controllers/CalorieTrackerViewController.swift
@@ -7,24 +7,39 @@
 //
 
 import UIKit
+import SwiftChart
 
 class CalorieTrackerViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var calorieChart: Chart!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    @IBAction func addButtonTapped(_ sender: Any) {
     }
-    */
+    
 
+}
+
+extension CalorieTrackerViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
