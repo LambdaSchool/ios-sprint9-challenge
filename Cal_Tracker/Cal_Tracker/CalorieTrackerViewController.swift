a057b91ae9a08ebe584eeb77e8eb0a77cafd7d42
Lydia Zhang <art4everyan@gmail.com>
2020-04-24 11:40:24 -0400

    Set Ups 2

---
+++ Cal_Tracker/Cal_Tracker/CalorieTrackerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalorieTrackerViewController.swift
+//  Cal_Tracker
+//
+//  Created by Lydia Zhang on 4/24/20.
+//  Copyright © 2020 Lydia Zhang. All rights reserved.
+//
+
+import UIKit
+import SwiftChart
+
+class CalorieTrackerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
