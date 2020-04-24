d359e39d7b278f7e37c9f5a357d6fcd24ffe2e8e
Ezra Black <dev.ezrablack@gmail.com>
2020-04-24 16:18:18 -0500

    Added Storyboard Views- Added Controller Files

---
+++ CalorieTracker/CalorieTracker/Controllers/CalorieController.swift
@@ -0,0 +1,9 @@
+//
+//  CalorieController.swift
+//  CalorieTracker
+//
+//  Created by Ezra Black on 4/24/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import Foundation
---
+++ CalorieTracker/CalorieTracker/Controllers/Notifications+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Notifications+Extension.swift
+//  CalorieTracker
+//
+//  Created by Ezra Black on 4/24/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import Foundation
+
+
+extension Notification.Name {
+    static var calorieListUpdated = Notification.Name("calorieListUpdated")
+}
