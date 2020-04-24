b6f26ec6bcba7020de5ae53d1388ce26364d6992
Shawn Gee <shawngee@gmail.com>
2020-04-24 13:17:41 -0400

    Implemented chart view controller

--- CalorieTracker/CalorieTracker/ViewControllers/CalorieTrackerChartViewController.swift
+++ CalorieTracker/CalorieTracker/ViewControllers/CalorieTrackerChartViewController.swift
@@ -6,12 +6,16 @@
 //  Copyright © 2020 Swift Student. All rights reserved.
 //
 
+import CoreData
 import SwiftChart
 import UIKit
 
 class CalorieTrackerChartViewController: UIViewController {
     
+    typealias Coordinate = (x: Int, y: Double)
+    
     // MARK: - Private Properties
+    
     private var chartView: Chart!
     
     // MARK: - View Lifecycle
@@ -23,7 +27,33 @@ class CalorieTrackerChartViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpChart()
+        updateChart(nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateChart(_:)), name: .calorieEntryAdded, object: nil)
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setUpChart() {
+        chartView.minY = 0
+    }
+    
+    @objc private func updateChart(_ notification: Notification?) {
+        let fetchRequest: NSFetchRequest<CalorieEntry> = CalorieEntry.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: true)]
+        
+        do {
+            let calorieEntries = try CoreDataStack.shared.mainContext.fetch(fetchRequest)
+            let coordinates = calorieEntries.reduce([]) { previousResult, calorieEntry -> [Coordinate] in
+                let coordinate = Coordinate(previousResult.count, Double(calorieEntry.calories))
+                return previousResult + [coordinate]
+            }
+            
+            let series = ChartSeries(data: coordinates)
+            series.area = true
+            chartView.series = [series]
+        } catch {
+            print(error)
+        }
     }
 }
