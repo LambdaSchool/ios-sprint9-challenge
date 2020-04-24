cd14698fd7132b33bc3207648f64fef36bc8d4b2
Ezra Black <dev.ezrablack@gmail.com>
2020-04-24 18:39:17 -0500

    Refactored my CoredataStack Snippet. Updated pod files

--- CalorieTracker/CalorieTracker/Controllers/CalorieController.swift
+++ CalorieTracker/CalorieTracker/Controllers/CalorieController.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import CoreData
+
+class CalorieController {
+    func create(amount: Int,
+                timestamp: Date = Date(),
+                context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        _ = Calorie(amount: amount, timestamp: timestamp, context: context)
+        context.perform {
+            do {
+                try CoreDataStack.shared.save(context: context)
+                NotificationCenter.default.post(name: .calorieListUpdated, object: nil)
+            } catch {
+                print("Unable to save: \(error)")
+                context.reset()
+            }
+        }
+    }
+
+    func delete(calorie: Calorie,
+                context: NSManagedObjectContext = CoreDataStack.shared.mainContext,
+                completion: @escaping (Error?) -> Void = { _ in }) {
+        context.perform {
+            do {
+                context.delete(calorie)
+                try CoreDataStack.shared.save(context: context)
+                NotificationCenter.default.post(name: .calorieListUpdated, object: nil)
+            } catch {
+                print("Could not save after deletion: \(error)")
+                context.reset()
+            }
+        }
+    }
+}
