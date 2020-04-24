2cfc612934d953d0a7309565ab9b7617bc0f8676
Wyatt Harrell <wyattaharrell@gmail.com>
2020-04-24 11:57:44 -0400

    Saving to CoreData tested

--- Calorie Tracker/Calorie Tracker/View Controllers/CalorieTrackerViewController.swift
+++ Calorie Tracker/Calorie Tracker/View Controllers/CalorieTrackerViewController.swift
@@ -8,15 +8,16 @@
 
 import UIKit
 import SwiftChart
+import CoreData
 
 class CalorieTrackerViewController: UIViewController {
 
     // MARK: - IBOutlets
-    @IBOutlet weak var calorieChart: Chart!
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet private weak var calorieChart: Chart!
+    @IBOutlet private weak var tableView: UITableView!
     
     // MARK: - Properties
-    
+    var entries: [Double] = []
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
@@ -31,14 +32,24 @@ class CalorieTrackerViewController: UIViewController {
         
         var calorieTextfield: UITextField!
         
-        alert.addTextField { (textField) in
+        alert.addTextField { textField in
             calorieTextfield = textField
             textField.placeholder = "Calories:"
         }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        let submitAction = UIAlertAction(title: "Submit", style: .default) { (_) in
-            // Add Calorie Intake to Core Data Here
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { _ in
+            guard let number = calorieTextfield.text, let numberOfCalorories = Double(number) else { return }
+            
+            Entry(numberOfCalories: numberOfCalorories)
+            
+            do {
+                try CoreDataStack.shared.save()
+            } catch {
+                NSLog("Error saving managed object contect: \(error)")
+            }
+            
+            self.tableView.reloadData()
         }
         
         alert.addAction(cancelAction)
@@ -47,23 +58,38 @@ class CalorieTrackerViewController: UIViewController {
         present(alert, animated: true)
     }
     
+    lazy var fetchedResultsController: NSFetchedResultsController<Entry> = {
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: true),
+                                        NSSortDescriptor(key: "numberOfCalories", ascending: true)]
+        let context = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: "timestamp", cacheName: nil)
+        frc.delegate = self
+        do {
+            try frc.performFetch()
+        } catch {
+            NSLog("Error performing fetch \(error)")
+        }
+        return frc
+    }()
 
 }
 
 extension CalorieTrackerViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+       return fetchedResultsController.sections?.count ?? 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CalorieCell", for: indexPath)
 
-        cell.textLabel?.text = "Hello"
-        cell.detailTextLabel?.text = "Test"
+        let entry = fetchedResultsController.object(at: indexPath)
+        cell.textLabel?.text = "Calories: \(entry.numberOfCalories)"
+        cell.detailTextLabel?.text = "\(entry.timestamp)"
 
         return cell
     }
@@ -79,4 +105,53 @@ extension CalorieTrackerViewController: UITableViewDelegate, UITableViewDataSour
         }
     }
     */
+}
+
+extension CalorieTrackerViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            break
+        }
+    }
 }
