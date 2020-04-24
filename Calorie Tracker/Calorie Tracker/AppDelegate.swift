e330d3bc93d46c2cea4f24d85a92ae46e8993764
Hector L <hectoremil21@hotmail.com>
2020-04-24 14:18:06 -0400

    Removed swift ignore from app delegate

--- Calorie Tracker/Calorie Tracker/AppDelegate.swift
+++ Calorie Tracker/Calorie Tracker/AppDelegate.swift
@@ -11,8 +11,6 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    // swiftlint:disable all
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -32,5 +30,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-    // swiftlint:enable all
 }
