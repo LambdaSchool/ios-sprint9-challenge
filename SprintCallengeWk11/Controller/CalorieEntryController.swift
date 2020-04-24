30f87710492c1f3bb345a02394ba86f7dd24fc17
Brad <braddiroff@icloud.com>
2020-04-24 11:43:58 -0400

    Built view controller

---
+++ SprintCallengeWk11/Controller/CalorieEntryController.swift
@@ -0,0 +1,12 @@
+//
+//  CalorieEntryController.swift
+//  SprintCallengeWk11
+//
+//  Created by Bradley Diroff on 4/24/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
