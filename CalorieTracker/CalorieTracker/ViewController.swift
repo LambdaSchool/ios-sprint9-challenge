7ed66b045a61935714656e38202ee7ce027c3628
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-24 16:53:38 +0100

    Swiftlint Incorporated

---
+++ CalorieTracker/CalorieTracker/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CalorieTracker
+//
+//  Created by Tobi Kuyoro on 24/04/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
