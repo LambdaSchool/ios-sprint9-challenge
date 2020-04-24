aa4523622933080cc2714f487297c0d2172003f8
Shawn Gee <shawngee@gmail.com>
2020-04-24 11:27:34 -0400

    Renamed ViewController to CalorieTrackerViewController

--- CalorieTracker/CalorieTracker/CalorieTrackerViewController.swift
+++ CalorieTracker/CalorieTracker/CalorieTrackerViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CalorieTrackerViewController.swift
 //  CalorieTracker
 //
 //  Created by Shawn Gee on 4/24/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalorieTrackerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
