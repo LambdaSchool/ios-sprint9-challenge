e7ca0ee972517febd83eca6007c1c7360503e4f2
Mark Gerrior <github@gerrior.com>
2020-04-24 14:44:18 -0700

    Added CalorieTableViewCell

---
+++ Calorie Tracker/Calorie Tracker/CalorieTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalorieTableViewCell.swift
+//  Calorie Tracker
+//
+//  Created by Mark Gerrior on 4/24/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class CalorieTableViewCell: UITableViewCell {
+
+}
