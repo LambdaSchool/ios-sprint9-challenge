3695fafeeb7ad022db55ce17c1a438a0b29a3612
Jessie <jessie.griffin@icloud.com>
2020-05-01 21:51:17 -0400

    Adds all CoreData files

---
+++ CalorieTracker/CalorieTracker/Model/CalorieIntakeRepresentation.swift
@@ -0,0 +1,9 @@
+//
+//  CalorieIntakeRepresentation.swift
+//  CalorieTracker
+//
+//  Created by Jessie Ann Griffin on 5/1/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import Foundation
