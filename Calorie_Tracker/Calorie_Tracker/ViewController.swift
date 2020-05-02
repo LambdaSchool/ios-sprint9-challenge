7c7802361e5fe62a7e38ab3fab218c328fd8ea55
Aaron Haughton <dreamcast2000@outlook.com>
2020-05-02 15:03:38 -0400

    Initial Commit

---
+++ Calorie_Tracker/Calorie_Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calorie_Tracker
+//
+//  Created by Joe on 5/2/20.
+//  Copyright © 2020 AlphaGradeINC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
