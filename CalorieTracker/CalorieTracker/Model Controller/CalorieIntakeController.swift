22eff83deb3c196359fe21c44a66208e2ef21a08
Jessie <jessie.griffin@icloud.com>
2020-05-01 23:00:12 -0400

    Revises alert and adds CRUD methods to Controller

--- CalorieTracker/CalorieTracker/Model Controller/CalorieIntakeController.swift
+++ CalorieTracker/CalorieTracker/Model Controller/CalorieIntakeController.swift
@@ -11,4 +11,40 @@ import CoreData
 
 class CalorieIntakeController {
 
+    var listOfIntakes: [CalorieIntake] {
+        return loadFromPersistentStore()
+    }
+
+    func createIntake(withCalories: Int16) {
+        _ = CalorieIntake(calories: withCalories, time: Date())
+        saveToPersistentStore()
+    }
+
+    func delete(intake: CalorieIntake) {
+        CoreDataStack.shared.mainContext.delete(intake)
+        saveToPersistentStore()
+    }
+
+    func loadFromPersistentStore() -> [CalorieIntake] {
+
+        let fetchRequest: NSFetchRequest<CalorieIntake> = CalorieIntake.fetchRequest()
+
+        fetchRequest.predicate = NSPredicate(format: "date == %@", Date() as CVarArg)
+        let moc = CoreDataStack.shared.mainContext
+
+        do {
+            return try moc.fetch(fetchRequest)
+        } catch {
+            print("Error fetching from moc: \(error)")
+            return []
+        }
+    }
+
+    func saveToPersistentStore() {
+        do {
+            try CoreDataStack.shared.mainContext.save()
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
+        }
+    }
 }
--- CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerViewController.swift
+++ CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerViewController.swift
@@ -14,6 +14,8 @@ class CalorieTrackerViewController: UIViewController {
     @IBOutlet weak var intakeTableView: UITableView!
     @IBOutlet weak var intakeGraphView: UIView!
 
+    let calorieIntakeController = CalorieIntakeController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,12 +27,18 @@ class CalorieTrackerViewController: UIViewController {
         let alert = UIAlertController(title: "Add Calorie Intake",
                                       message: "Add the amount of calories in the field",
                                       preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
         alert.addTextField { textField in
             textField.placeholder = "Calories:"
         }
-
-        alert.addAction(UIAlertAction(title: "Submit", style: .default, handler: nil))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        //swiftlint:disable:next unused_closure_parameter
+        alert.addAction(UIAlertAction(title: "Submit", style: .default, handler: { action in
+            if let caloriesText = alert.textFields?.first?.text, let calories = Int16(caloriesText) {
+                self.calorieIntakeController.createIntake(withCalories: calories)
+            }
+        }))
 
         self.present(alert, animated: true)
     }
