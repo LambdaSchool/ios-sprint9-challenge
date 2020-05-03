1f9063a513f075885450fb916d9b0428a3b91d5f
Aaron Haughton <dreamcast2000@outlook.com>
2020-05-03 10:59:27 -0400

    Added MVC and Tableviewcontroller

---
+++ Calorie_Tracker/Calorie_Tracker/Models/CalorieEntry.swift
@@ -0,0 +1,13 @@
+//
+//  CalorieEntry.swift
+//  Calorie_Tracker
+//
+//  Created by Joe on 5/3/20.
+//  Copyright © 2020 AlphaGradeINC. All rights reserved.
+//
+
+import Foundation
+
+struct CalorieEntry {
+    
+}
