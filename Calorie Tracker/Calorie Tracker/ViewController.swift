55cbe0cdcab19041e9db459c20a70fc94caa1b62
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-03 17:26:00 -0400

    Initial Commit

---
+++ Calorie Tracker/Calorie Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calorie Tracker
+//
+//  Created by Christy Hicks on 5/3/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
