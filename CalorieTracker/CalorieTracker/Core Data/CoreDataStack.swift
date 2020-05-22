6c49334f4acc1fc5cbaa9530b74d579d95d2eef4
Chris Dobek <dobek.christopher@gmail.com>
2020-05-22 12:55:45 -0400

    App is working

--- CalorieTracker/CalorieTracker/Core Data/CoreDataStack.swift
+++ CalorieTracker/CalorieTracker/Core Data/CoreDataStack.swift
@@ -18,7 +18,7 @@ class CoreDataStack {
     }
     
     lazy var container: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "CalorieTracker")
+        let container = NSPersistentContainer(name: "CalorieIntake")
         container.loadPersistentStores { _, error in
             if let error = error {
                 fatalError("Failed to load persistent stores: \(error)")
--- CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerTableViewController.swift
+++ CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerTableViewController.swift
@@ -15,76 +15,106 @@ class CalorieTrackerTableViewController: UITableViewController {
     
     @IBOutlet weak var chart: Chart!
     
+    var calorieIntakeArray: [CalorieIntake] {
+        let fetchRequest: NSFetchRequest<CalorieIntake> = CalorieIntake.fetchRequest()
+        let context = CoreDataStack.shared.mainContext
+        do {
+            return try context.fetch(fetchRequest)
+        } catch {
+            NSLog("Error fetching calorieIntakes: \(error)")
+            return []
+        }
+    }
+    
+    var calorieSeriesArray: ChartSeries {
+        var temp: [Double] = []
+        for intake in calorieIntakeArray {
+            temp.append(Double(intake.calories))
+        }
+        return ChartSeries(temp)
+    }
+    
+    fileprivate var textField: UITextField?
+    
+    fileprivate lazy var alertController: UIAlertController = {
+        let alert = UIAlertController(title: "Add Calorie Intake.", message: "Enter the number of calories below.", preferredStyle: .alert)
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let submitButton = UIAlertAction(title: "Submit", style: .default) { _ in
+            print(self.textField?.text ?? "")
+            self.saveCalorieIntake()
+            self.textField?.text = ""
+            self.saveCalorieIntake()
+            NotificationCenter.default.post(name: .updateName, object: self)
+        }
+        alert.addAction(cancelButton)
+        alert.addAction(submitButton)
+        alert.addTextField { textField in
+            self.textField = textField
+        }
+        return alert
+    }()
+    
+    var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .short
+        return df
+    }()
+    
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        present(alertController, animated: true)
+    }
+    
+    func saveCalorieIntake() {
+        guard let calories = textField!.text, !calories.isEmpty else { return }
+
+        let cals: Int = Int(calories) ?? 0
+        _ = CalorieIntake(calories: cals, dateEntered: Date())
+
+        do {
+            try CoreDataStack.shared.mainContext.save()
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
+        }
+    }
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(refresh), name: .updateName, object: nil)
+
+        chart.add(calorieSeriesArray)
 
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
+    
+    
+    @objc func refresh() {
+           chart.removeAllSeries()
+           chart.add(calorieSeriesArray)
+           tableView.reloadData()
+    }
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return calorieIntakeArray.count
     }
 
-    /*
+  
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CalorieCell", for: indexPath)
+        
+        let calorieIntake = calorieIntakeArray[indexPath.row]
+        cell.textLabel?.text = "Calories: \(calorieIntake.calories)"
+        cell.detailTextLabel?.text = dateFormatter.string(from: calorieIntake.dateEntered ?? Date())
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
