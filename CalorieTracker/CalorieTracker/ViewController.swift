9fa52ef8506f9b004e1fb190bd3e843db1d5961e
Hunter Oppel <hunter.oppel@gmail.com>
2020-05-22 11:08:24 -0400

    Added calorie tracker project.

---
+++ CalorieTracker/CalorieTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieTracker
+//
+//  Created by Hunter Oppel on 5/22/20.
+//  Copyright © 2020 LambdaSchool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
