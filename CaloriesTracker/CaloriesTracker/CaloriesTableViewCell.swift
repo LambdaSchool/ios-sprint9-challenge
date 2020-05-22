53bd7b330d45e72b20a87eb244ed43682376dc41
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-22 08:51:29 -0700

    Added Calories table view cell

---
+++ CaloriesTracker/CaloriesTracker/CaloriesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CaloriesTableViewCell.swift
+//  CaloriesTracker
+//
+//  Created by Bhawnish Kumar on 5/22/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class CaloriesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
