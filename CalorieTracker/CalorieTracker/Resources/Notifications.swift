9f3db188f21aabdb49e64fc2abb4ce6cf388066e
Chris Dobek <dobek.christopher@gmail.com>
2020-05-22 11:38:11 -0400

    Notification file created

---
+++ CalorieTracker/CalorieTracker/Resources/Notifications.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  CalorieTracker
+//
+//  Created by Chris Dobek on 5/22/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    static let updateName = NSNotification.Name("updateName")
+}
