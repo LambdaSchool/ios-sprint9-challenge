72c75ceccefc5d720ebcda0bcdf70206095a46f4
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-22 09:13:33 -0700

    added Calories+Convenience & fixed info.plist file location.

---
+++ CaloriesTracker/CaloriesTracker/Model/Calories+Convenience.swift
@@ -0,0 +1,12 @@
+//
+//  Calories+Convenience.swift
+//  CaloriesTracker
+//
+//  Created by Bhawnish Kumar on 5/22/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
