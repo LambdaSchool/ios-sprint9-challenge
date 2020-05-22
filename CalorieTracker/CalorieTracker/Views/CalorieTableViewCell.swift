d10ed554b51154086141f5d3379b1a7d4a74e294
Brad <braddiroff@icloud.com>
2020-05-22 12:11:45 -0400

    Made view, still working on it

---
+++ CalorieTracker/CalorieTracker/Views/CalorieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CalorieTableViewCell.swift
+//  CalorieTracker
+//
+//  Created by Bradley Diroff on 5/22/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+import UIKit
+
+class CalorieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
