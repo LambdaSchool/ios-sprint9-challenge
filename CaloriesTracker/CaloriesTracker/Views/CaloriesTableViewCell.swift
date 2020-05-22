d252a9c784508e9eebc5974e3ba9739c4e768df1
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-22 10:45:58 -0700

    Assigned data to the iboutlets

--- CaloriesTracker/CaloriesTracker/Views/CaloriesTableViewCell.swift
+++ CaloriesTracker/CaloriesTracker/Views/CaloriesTableViewCell.swift
@@ -9,19 +9,38 @@
 import UIKit
 
 class CaloriesTableViewCell: UITableViewCell {
-
+    
+    var calories: Calories? {
+        didSet {
+            updatesViews()
+        }
+    }
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US")
+        formatter.dateFormat = "MMM d, yyyy"
+        return formatter
+    }()
+    
+    private let timeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US")
+        formatter.dateFormat = "h:mm a"
+        return formatter
+    }()
+    
     @IBOutlet weak var calorieLabel: UILabel!
     @IBOutlet weak var timestampLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    func updatesViews() {
+        guard let calories = calories else { return }
+        
+        let dateString = dateFormatter.string(from: calories.timestamp!)
+        let timeString = timeFormatter.string(from: calories.timestamp!)
+        timestampLabel.text = "\(dateString) at \(timeString)"
+        calorieLabel.text = "Calories: \(calories.calories)"
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }
