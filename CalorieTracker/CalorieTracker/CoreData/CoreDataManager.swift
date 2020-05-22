773eb63b93b4a45be8da0b9c8fd4c560907023bd
Shawn James <shawn.james@me.com>
2020-05-22 11:46:31 -0400

    Add CoreData singleton, container, and context

---
+++ CalorieTracker/CalorieTracker/CoreData/CoreDataManager.swift
@@ -0,0 +1,32 @@
+//
+//  CoreDataManager.swift
+//  CalorieTracker
+//
+//  Created by Shawn James on 5/22/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    // create singleton
+    static let shared = CoreDataManager()
+    
+    // create container
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Tasks")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    // create context -> everything done in the app uses the context
+    var mainContext: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+}
