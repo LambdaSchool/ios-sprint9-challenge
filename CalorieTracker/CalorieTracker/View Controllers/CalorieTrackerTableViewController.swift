14fd74a285c32584538cf9a925a09b4d021617b4
Chris Dobek <dobek.christopher@gmail.com>
2020-05-22 12:10:27 -0400

    Added CoreData model

--- CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerTableViewController.swift
+++ CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerTableViewController.swift
@@ -12,15 +12,12 @@ import SwiftChart
 
 
 class CalorieTrackerTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var chart: Chart!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
