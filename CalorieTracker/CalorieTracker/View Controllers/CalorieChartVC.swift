75baeff3a651f0a5f1ddff54dab675068a244591
Chad Parker <chad@chadparker.com>
2020-06-12 19:29:38 -0700

    Set up View Controllers and storyboard

---
+++ CalorieTracker/CalorieTracker/View Controllers/CalorieChartVC.swift
@@ -0,0 +1,30 @@
+//
+//  CalorieChartVC.swift
+//  CalorieTracker
+//
+//  Created by Chad Parker on 6/12/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class CalorieChartVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
