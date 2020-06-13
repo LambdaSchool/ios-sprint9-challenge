85d08db2078f0b9be282ae5bed5abc143f648a0a
Chad Parker <chad@chadparker.com>
2020-06-12 21:52:38 -0700

    Correct indentation

--- CalorieTracker/CalorieTracker/Model/CoreDataStack.swift
+++ CalorieTracker/CalorieTracker/Model/CoreDataStack.swift
@@ -10,37 +10,37 @@ import Foundation
 import CoreData
 
 class CoreDataStack {
-    static let shared = CoreDataStack()
-    
-    lazy var container: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "CalorieTracker")
-        container.loadPersistentStores { _, error in
-            if let error = error {
-                fatalError("Failed to load persistent stores: \(error)")
-            }
-        }
-        container.viewContext.automaticallyMergesChangesFromParent = true
-        return container
-    }()
-    
-    var mainContext: NSManagedObjectContext {
-        container.viewContext
-    }
-    
-    // This lets us save any context on the right thread, avoiding concurrency issues.
-    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
-        var error: Error?
-        
-        context.performAndWait {
-            do {
-                try context.save()
-            } catch let saveError as NSError {
-                error = saveError
-            }
-        }
-        
-        if let error = error {
-            throw error
-        }
-    }
+   static let shared = CoreDataStack()
+   
+   lazy var container: NSPersistentContainer = {
+      let container = NSPersistentContainer(name: "CalorieTracker")
+      container.loadPersistentStores { _, error in
+         if let error = error {
+            fatalError("Failed to load persistent stores: \(error)")
+         }
+      }
+      container.viewContext.automaticallyMergesChangesFromParent = true
+      return container
+   }()
+   
+   var mainContext: NSManagedObjectContext {
+      container.viewContext
+   }
+   
+   // This lets us save any context on the right thread, avoiding concurrency issues.
+   func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+      var error: Error?
+      
+      context.performAndWait {
+         do {
+            try context.save()
+         } catch let saveError as NSError {
+            error = saveError
+         }
+      }
+      
+      if let error = error {
+         throw error
+      }
+   }
 }
