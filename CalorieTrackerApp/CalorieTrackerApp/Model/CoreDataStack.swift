b7c93c7a5a7e6b7bd412766271b6b6e41d71ab56
Jarren Campos <jarrenmcampos@gmail.com>
2020-06-12 19:38:48 -0700

    Added Core data files

---
+++ CalorieTrackerApp/CalorieTrackerApp/Model/CoreDataStack.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataStack.swift
+//  CalorieTrackerApp
+//
+//  Created by Jarren Campos on 6/12/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CalorieEntry")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        container.viewContext
+    }
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var saveError: Error?
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch {
+                saveError = error
+            }
+        }
+        if let error = saveError { throw error }
+    }
+}
