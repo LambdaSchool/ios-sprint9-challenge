773a6a78f8d6d57447594ce07daabc304952d944
Claudia Maciel <clc_80@yahoo.com>
2020-06-12 18:23:26 -0700

    Added CoreDataStack file

---
+++ CalorieTracker/CalorieTracker/Model/CoreDataStack.swift
@@ -0,0 +1,32 @@
+//
+//  CoreDataStack.swift
+//  CalorieTracker
+//
+//  Created by Claudia Contreras on 6/12/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    // This is a shared instance of the Core Data Stack
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+       let container = NSPersistentContainer(name: "Calories")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    // Makes the access to the context faster
+    // Reminds you to use the context on the main queue
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
