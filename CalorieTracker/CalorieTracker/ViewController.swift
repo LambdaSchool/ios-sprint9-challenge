8866fb372211bf19f459dd9361a01d8fa6e497db
Jarren Campos <jarrenmcampos@gmail.com>
2020-06-12 18:33:16 -0700

    Added initial Project files

---
+++ CalorieTracker/CalorieTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieTracker
+//
+//  Created by Jarren Campos on 6/12/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
