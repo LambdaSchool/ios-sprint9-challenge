adcf89044e6f4365e849248cd1fc5c210c5a94df
Chad Parker <chad@chadparker.com>
2020-06-12 19:44:27 -0700

    Set outlets, clean up VCs

--- CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerVC.swift
+++ CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerVC.swift
@@ -9,22 +9,19 @@
 import UIKit
 
 class CalorieTrackerVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+   
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      switch segue.identifier {
+      case "ChartSegue":
+         break
+      case "TableViewSegue":
+         break
+      default:
+         break
+      }
+   }
 }
