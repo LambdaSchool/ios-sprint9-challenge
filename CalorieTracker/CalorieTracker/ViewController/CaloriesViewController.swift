57d1d19f1d2d851c167e5f28a9d991d10e4e9987
Claudia Maciel <clc_80@yahoo.com>
2020-06-12 19:51:28 -0700

    Core Data fetch set up

--- CalorieTracker/CalorieTracker/ViewController/CaloriesViewController.swift
+++ CalorieTracker/CalorieTracker/ViewController/CaloriesViewController.swift
@@ -14,7 +14,22 @@ class CaloriesViewController: UIViewController {
 
     // MARK: - Properties
     var calorieEntries: [Int16] = []
+    let dateFormatter = DateFormatter()
     
+    lazy var fetchedResultsController: NSFetchedResultsController<Calories> = {
+        let fetchRequest: NSFetchRequest<Calories> = Calories.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: true),
+                                        NSSortDescriptor(key: "amount", ascending: true)]
+        let context = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: "date", cacheName: nil)
+        frc.delegate = self
+        do {
+            try frc.performFetch()
+        } catch {
+            NSLog("Error performing fetch \(error)")
+        }
+        return frc
+    }()
     
     // MARK: - IBOutlets
     @IBOutlet var tableView: UITableView!
@@ -61,21 +76,78 @@ class CaloriesViewController: UIViewController {
         
         present(alertController, animated: true)
     }
+    
 
 }
 
+// MARK: - Extensions
+
 extension CaloriesViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CaloriesCell", for: indexPath)
-        cell.textLabel?.text = "Calories:"
-        cell.detailTextLabel?.text = "Date"
+        
+        dateFormatter.dateFormat = "MMM d, yyyy 'at' h:mm a"
+        
+        let calories = fetchedResultsController.object(at: indexPath)
+        cell.textLabel?.text = "Calories: \(calories.amount)"
+        cell.detailTextLabel?.text = "\(dateFormatter.string(from: calories.date!))"
         
         return cell
     }
-    
-    
+}
+
+extension CaloriesViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            break
+        }
+    }
 }
