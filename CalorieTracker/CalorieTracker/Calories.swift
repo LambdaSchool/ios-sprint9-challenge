d19499002a3b4bdccf7715841808c6710c19e08c
kensam1 <samarakodykennedy@gmail.com>
2020-06-12 23:56:26 -0400

    Finished UI

---
+++ CalorieTracker/CalorieTracker/Calories.swift
@@ -0,0 +1,14 @@
+//
+//  Calories.swift
+//  CalorieTracker
+//
+//  Created by Lambda_School_Loaner_241 on 6/12/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+import Foundation
+
+struct Calories {
+    let calInt: Int
+    let date: Date
+}
