de83d30c653313e40cb650f59bdd65f532c4870f
Claudia Maciel <clc_80@yahoo.com>
2020-06-12 18:30:02 -0700

    Added IBOutlets and IBActions

---
+++ CalorieTracker/CalorieTracker/ViewController/CaloriesViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CaloriesViewController.swift
+//  CalorieTracker
+//
+//  Created by Claudia Contreras on 6/12/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class CaloriesViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet var tableView: UITableView!
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - IBAction
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
