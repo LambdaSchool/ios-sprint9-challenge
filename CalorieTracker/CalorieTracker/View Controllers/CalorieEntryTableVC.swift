5ffabcf47f10cefca5ed271f1affdd7cdfa64088
Chad Parker <chad@chadparker.com>
2020-06-12 19:55:54 -0700

    Add CoreDataStack

--- CalorieTracker/CalorieTracker/View Controllers/CalorieEntryTableVC.swift
+++ CalorieTracker/CalorieTracker/View Controllers/CalorieEntryTableVC.swift
@@ -17,13 +17,14 @@ class CalorieEntryTableVC: UITableViewController {
    // MARK: - Table view data source
    
    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return 0
+      0
    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "CalorieEntryCell", for: indexPath)
-    
-    // Configure the cell...
-    
-    return cell
-    }
+   
+   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "CalorieEntryCell", for: indexPath)
+      
+      // Configure the cell...
+      
+      return cell
+   }
 }
