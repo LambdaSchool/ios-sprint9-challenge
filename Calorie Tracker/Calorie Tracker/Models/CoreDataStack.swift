f8dec24e62da8ecad906e4380cbacb7dec6dedcb
Thomas Dye <thomasallendye@gmail.com>
2020-06-14 21:41:02 -0400

    Created core data stack

---
+++ Calorie Tracker/Calorie Tracker/Models/CoreDataStack.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataStack.swift
+//  Calorie Tracker
+//
+//  Created by Thomas Dye on 6/14/20.
+//  Copyright © 2020 Thomas Dye. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+
+    lazy var container: NSPersistentContainer = {
+       let container = NSPersistentContainer(name: "CalorieCounter")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+
+    var mainContext: NSManagedObjectContext {
+        let context = container.viewContext
+        context.automaticallyMergesChangesFromParent = true
+        return context
+    }
+
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var error: Error?
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+
+}
