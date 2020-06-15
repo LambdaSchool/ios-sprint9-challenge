cb0f39c3114d6f85a555568c11d540bf71ba22af
Matt Martindale <martindalematt18@yahoo.com>
2020-06-14 19:18:29 -0600

    installed SwiftChart and laid out storyboard

--- Calorie Tracker/Calorie Tracker/View Controllers/ViewController.swift
+++ Calorie Tracker/Calorie Tracker/View Controllers/ViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import SwiftChart
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showChart()
     }
 
+    func showChart() {
+        let chart = Chart(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+        let series = ChartSeries([0, 6.5, 2, 8, 4.1, 7, -3.1, 10, 8])
+        chart.add(series)
+    }
 
 }
 
