ef7b3ff9d35dee7259427c36653c62ab1ffce0b5
John Holowesko <john@sipsip.io>
2020-06-14 21:21:24 -0400

    Initial Commit

---
+++ Calorie Tracker/Calorie Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calorie Tracker
+//
+//  Created by John Holowesko on 6/14/20.
+//  Copyright © 2020 John Holowesko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
