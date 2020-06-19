188043e654922864abd27b6717ee8e48901d2c74
Harmony Radley <harmonyradley99@gmail.com>
2020-06-19 19:56:49 -0400

    Almost MVP

--- CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerTableViewController.swift
+++ CalorieTracker/CalorieTracker/View Controllers/CalorieTrackerTableViewController.swift
@@ -11,7 +11,12 @@ import CoreData
 import SwiftChart
 
 class CalorieTrackerTableViewController: UIViewController {
+    // MARK: - IBOutlets
 
+    @IBOutlet var chart: Chart!
+    @IBOutlet var tableView: UITableView!
+
+    // MARK: - Properties
 
     lazy var fetchedResultsController: NSFetchedResultsController<Calorie> = {
         let fetchRequest: NSFetchRequest<Calorie> = Calorie.fetchRequest()
@@ -19,48 +24,130 @@ class CalorieTrackerTableViewController: UIViewController {
             NSSortDescriptor(key: "date", ascending: true)
         ]
         let context = CoreDataStack.shared.mainContext
-        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,                                                                          managedObjectContext: context,
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,                                                                                       managedObjectContext: context,
                                              sectionNameKeyPath: nil, cacheName: nil)
         frc.delegate = self
         do {
             try frc.performFetch()
         } catch {
-            print("Error fetching: ", error)
+            print("Error fetching results: \(error)")
         }
         return frc
     }()
 
     private var dateFormatter: DateFormatter = {
-           let formatter = DateFormatter()
-           formatter.dateStyle = .short
-           formatter.timeStyle = .medium
-           return formatter
-       }()
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        updateViews()
     }
 
+    // MARK: - IBAction
+    @IBAction func addButtonTapped(_ sender: Any) {
+        createCalorieAlert()
+    }
 
+    // MARK: - Methods
+    @objc private func updateViews() {
+        tableView.reloadData()
+        updateChart()
+    }
+
+    private func updateChart() {
+        var count: [Double] = []
+        let calories = fetchedResultsController.fetchedObjects ?? []
+
+        for calories in calories {
+            let calories = Double(calories.calorieAmount)
+            count.append(calories)
+        }
+
+        let chartSeries = ChartSeries(count)
+        chartSeries.area = true
+        chart.add(chartSeries)
+    }
+
+    private func createCalorieAlert() {
+        let alert = UIAlertController(title: "Add Calorie Intake", message: "Enter the ammount of calories below:", preferredStyle: .alert)
+
+        alert.addTextField { textField in
+            textField.placeholder = "Calories:"
+            textField.keyboardType = .numberPad
+        }
+
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let submitButton = UIAlertAction(title: "Submit", style: .default) { (_) in
+            guard let textField = alert.textFields,
+                let calorieString = textField[0].text,
+                !calorieString.isEmpty,
+                let calories = Double(calorieString) else { return }
+
+            self.saveCalories(calories)
+
+        }
+
+        alert.addAction(cancelButton)
+        alert.addAction(submitButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    private func saveCalories(_ calorieAmount: Double, date: Date = Date()) {
+        Calorie(calorieAmount: calorieAmount, date: date)
+        do {
+            try CoreDataStack.shared.save()
+        } catch {
+            print("Error saving to database: \(error)")
+        }
+
+        NotificationCenter.default.post(name: .chartUpdate, object: nil)
+    }
 }
 
-    // MARK: - Extension - Table view data source
+// MARK: - Extension - Table view data source
 
-    extension CalorieTrackerTableViewController: UITableViewDataSource {
-     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension CalorieTrackerTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CalorieCell", for: indexPath)
 
-        return 0
-    }
+        let calorieResults = fetchedResultsController.object(at: indexPath)
+        let calories = calorieResults.calorieAmount
+        let date = calorieResults.date ?? Date()
 
-
-     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-
+        cell.textLabel?.text = "Calories: \(Int(calories))"
+        cell.detailTextLabel?.text = "\(calorieResults.date ?? date)"
 
         return cell
     }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        fetchedResultsController.sections?[section].numberOfObjects ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let calorieData = fetchedResultsController.object(at: indexPath)
+            let context = CoreDataStack.shared.mainContext
+            do {
+                context.delete(calorieData)
+                try CoreDataStack.shared.save()
+                NotificationCenter.default.post(Notification(name: .chartUpdate, object: nil))
+            } catch {
+                context.reset()
+                print("Error deleting object from managed object context: \(error)")
+            }
+        }
+    }
+
+
+
 }
 
 
