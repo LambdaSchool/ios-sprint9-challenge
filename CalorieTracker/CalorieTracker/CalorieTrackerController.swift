fcb9e274d2fad55a04a2025fd0a6f98cf3543375
Harmony Radley <harmonyradley99@gmail.com>
2020-06-19 12:24:23 -0400

    Added some core data and notification

---
+++ CalorieTracker/CalorieTracker/CalorieTrackerController.swift
@@ -0,0 +1,9 @@
+//
+//  CalorieTrackerController.swift
+//  CalorieTracker
+//
+//  Created by Harmony Radley on 6/19/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import Foundation
---
+++ CalorieTracker/CalorieTracker/Notifications.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  CalorieTracker
+//
+//  Created by Harmony Radley on 6/19/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    static let chartUpdate = NSNotification.Name("chartUpdate")
+}
