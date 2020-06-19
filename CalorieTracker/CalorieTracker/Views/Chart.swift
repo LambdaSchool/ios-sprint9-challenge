b01698255b238fd2024717ddb66b1afd42ac6652
Cody Morley <codymorleyinc@gmail.com>
2020-06-19 08:27:15 -0700

    set a file to create a chart object

---
+++ CalorieTracker/CalorieTracker/Views/Chart.swift
@@ -0,0 +1,14 @@
+//
+//  Chart.swift
+//  CalorieTracker
+//
+//  Created by Cody Morley on 6/19/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import UIKit
+import SwiftChart
+
+class CalorieChart: Chart {
+    
+}
