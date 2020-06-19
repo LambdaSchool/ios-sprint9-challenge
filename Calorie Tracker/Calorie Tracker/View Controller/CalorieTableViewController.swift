e30fe949483d29f8f26a7c8e82091a4623f108fe
Vincent Hoang <vietnhoang7@gmail.com>
2020-06-19 10:37:14 -0700

    Add working chart and tableview implementations

---
+++ Calorie Tracker/Calorie Tracker/View Controller/CalorieTableViewController.swift
@@ -0,0 +1,130 @@
+//
+//  CalorieTableViewController.swift
+//  Calorie Tracker
+//
+//  Created by Vincent Hoang on 6/19/20.
+//  Copyright © 2020 Vincent Hoang. All rights reserved.
+//
+
+import UIKit
+import SwiftChart
+
+class CalorieTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //let notification = NSNotification.Name(rawValue: "reload")
+
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var chart: Chart!
+    
+    var df: DateFormatter = DateFormatter()
+    
+    var meals: [Meal] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        setUpDateFormatter()
+    }
+    
+    // MARK: - UITableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+        
+        let selectedMeal = meals[indexPath.row]
+        
+        let caloriesLabel = "Calories: \(selectedMeal.calories)"
+        let dateLabel = "\(df.string(from: selectedMeal.date))"
+        
+        cell.textLabel?.text = caloriesLabel
+        cell.detailTextLabel?.text = dateLabel
+        
+        return cell
+    }
+    
+    // MARK: - Actions
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        entryPrompt()
+    }
+    
+    // MARK: - Helper Functions
+    private func entryPrompt() {
+        let alert = UIAlertController(title: "Add Calorie Intake", message: "Enter the amount of calories in the field", preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Calories"
+            textField.keyboardType = .numberPad
+        })
+        
+        alert.addAction(
+            UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        alert.addAction(
+            UIAlertAction(title: "Submit", style: .default, handler: { _ in
+                guard let input = alert.textFields?[0].text else { return }
+                self.meals.append(Meal(calories: Int(input) ?? 0))
+                self.tableView.reloadData()
+                self.setUpChart()
+            }))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func setUpDateFormatter() {
+        df.dateStyle = .medium
+        df.timeStyle = .medium
+    }
+    
+    // MARK: - SwiftChart
+    private func setUpChart() {
+        let data = meals.map{ Double($0.calories) }
+        let series = ChartSeries(data)
+        series.area = true
+        
+        var xLabels: [Int] = []
+        xLabels.append(contentsOf: 0...meals.count)
+        
+        chart.xLabels = xLabels.map { Double($0) }
+        chart.xLabelsSkipLast = false
+        chart.xLabelsTextAlignment = .left
+
+        chart.add(series)
+        chart.minY = 0
+        
+        let y = Double(findMaxY(calories: findMaxCalories()))
+        chart.maxY = y
+        
+    }
+    
+    private func findMaxCalories() -> Int {
+        var max = 0
+        
+        for meal in meals {
+            if meal.calories > max {
+                max = meal.calories
+            }
+        }
+        
+        return max
+    }
+    
+    private func findMaxY(calories: Int) -> Int {
+        if calories < 500 { return 500 }
+        
+        var increment = 1000
+        
+        while increment / calories == 0 {
+            increment += 500
+        }
+        
+        return increment
+    }
+    
+
+}
