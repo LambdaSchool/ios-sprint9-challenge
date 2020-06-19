ab284aacce326e847833eaea1a646992dec61d63
Vincent Hoang <vietnhoang7@gmail.com>
2020-06-19 09:04:24 -0700

    Install framework

---
+++ Calorie Tracker/Calorie Tracker/Model/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  Calorie Tracker
+//
+//  Created by Vincent Hoang on 6/19/20.
+//  Copyright © 2020 Vincent Hoang. All rights reserved.
+//
+
+import Foundation
+
+struct Meal {
+    let calories: Int
+    let date: Date = Date()
+}
