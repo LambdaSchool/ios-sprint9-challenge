acf2bac9537f87834d894dae91282e3871efffa5
Kelson Hartle <kelson99hartle@icloud.com>
2020-06-19 09:31:29 -0600

    Added files for core data implementation

---
+++ CALORIE-TRACKER/CALORIE-TRACKER/CalorieIntakeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CalorieIntakeTableViewCell.swift
+//  CALORIE-TRACKER
+//
+//  Created by Kelson Hartle on 6/19/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+import UIKit
+
+class CalorieIntakeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CALORIE-TRACKER/CALORIE-TRACKER/CalorieTrackerTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CalorieTrackerTableViewController.swift
+//  CALORIE-TRACKER
+//
+//  Created by Kelson Hartle on 6/19/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+import UIKit
+import SwiftChart
+
+class CalorieTrackerTableViewController: UITableViewController {
+
+
+    
+    @IBOutlet weak var chart: Chart!
+    
+    
+    
+    
+    
+    
+    
+    
+}
