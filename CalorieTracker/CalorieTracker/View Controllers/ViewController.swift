79690ebaf0471625c7fd715739acc64f30c97c53
Cody Morley <codymorleyinc@gmail.com>
2020-06-19 08:10:51 -0700

    Started new project, organized document outline.

---
+++ CalorieTracker/CalorieTracker/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieTracker
+//
+//  Created by Bling Morley on 6/19/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
