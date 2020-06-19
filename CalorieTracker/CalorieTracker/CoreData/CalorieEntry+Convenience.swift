c64015466cac6846185dba5db1198ab4dc797f9a
Joe Veverka <marissagonzales@Marissas-MacBook-Pro.local>
2020-06-19 11:20:06 -0400

    added coredata model attributes, coredata stack, and convenience files

---
+++ CalorieTracker/CalorieTracker/CoreData/CalorieEntry+Convenience.swift
@@ -0,0 +1,13 @@
+//
+//  CalorieEntry+Convenience.swift
+//  CalorieTracker
+//
+//  Created by Joe Veverka on 6/19/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import Foundation
+
+extension CalorieEntry {
+    
+}
---
+++ CalorieTracker/CalorieTracker/CoreData/CoreDataStack.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataStack.swift
+//  CalorieTracker
+//
+//  Created by Joe Veverka on 6/19/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import Foundation
+
+class CoreDataStack {
+    
+}
