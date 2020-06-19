de6056c623797045fa60b91e733e1d15acf6f43e
Joe Veverka <marissagonzales@Marissas-MacBook-Pro.local>
2020-06-19 11:44:18 -0400

    added Alert helper to create alert to enter calories. as well as NSNotification.Name helper file

---
+++ CalorieTracker/CalorieTracker/Helpers/Alert.swift
@@ -0,0 +1,9 @@
+//
+//  Alert.swift
+//  CalorieTracker
+//
+//  Created by Joe Veverka on 6/19/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import Foundation
---
+++ CalorieTracker/CalorieTracker/Helpers/NSNotification.Name+IDs.swift
@@ -0,0 +1,9 @@
+//
+//  NSNotification.Name+IDs.swift
+//  CalorieTracker
+//
+//  Created by Joe Veverka on 6/19/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import Foundation
