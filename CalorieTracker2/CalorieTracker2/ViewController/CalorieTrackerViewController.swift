ec6eb9ee6558238c666a6d336013148e7b722c6c
Stephanie <user.name>
2020-06-19 21:40:51 -0700

    convenience init

--- CalorieTracker2/CalorieTracker2/ViewController/CalorieTrackerViewController.swift
+++ CalorieTracker2/CalorieTracker2/ViewController/CalorieTrackerViewController.swift
@@ -10,6 +10,24 @@ import Foundation
 import CoreData
 import SwiftChart
 
+/*
+your model, forgive the softball is;
+Entity: EntityName
+    caloriesPropertyName: Double
+    datePropertyName: Date
+
+thassit. you can do the whole app with two numbers.
+
+so you make a core data model
+write a core data stack with a shared instance(singleton)
+write a convenience init (protip. use the current date as the default value and you only need one property to init)
+Make a view controller for your project w/ a view that inherits from chart and a tableview that uses a right detail layout, write in outlets for both
+Put in a bar button item with system type 'add' and give it an IBAction
+extend to conform to tableview datasource and set the datasource
+write in methods that will grab data via a UI alert and add it to core data
+write in methods that add the data from core data to the views and update it when the user submits (use the notification center)
+*/
+
 class CalorieTrackerViewController: UIViewController {
     
     @IBOutlet weak var chartView: Chart!
@@ -19,8 +37,13 @@ class CalorieTrackerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let series = ChartSeries([1, 6, 2, 8, 4, 7, 3, 10, 8])
+        series.color = ChartColors.greenColor()
+        chartView.add(series)
+        
         caloriesTableView.dataSource = self
         caloriesTableView.delegate = self
+        
     }
     
 }
