fb0d272fd0dc004329b79ecc0fd7fe34dea58e14
Stephanie <user.name>
2020-06-19 19:55:40 -0700

    created folders and core data model

---
+++ CalorieTracker2/CalorieTracker2/Views/CalorieTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTableViewCell.swift
+//  CalorieTracker2
+//
+//  Created by Stephanie Ballard on 6/19/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class CalorieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var caloriesTextLabel: UILabel!
+    @IBOutlet weak var timestampTextLabel: UILabel!
+    
+
+}
