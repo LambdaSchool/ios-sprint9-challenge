43493e9b75d54b2f8fd30799344b190e88177d88
Libby Thomas <thomaslibby05@gmail.com>
2020-08-14 20:07:11 -0500

    Initial Commit

---
+++ CalorieTracker/CalorieTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieTracker
+//
+//  Created by Elizabeth Thomas on 8/14/20.
+//  Copyright © 2020 Libby Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
