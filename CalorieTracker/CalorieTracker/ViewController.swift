2e4bc677605103f5fa9d52ee594d984b1d9959e2
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-08-14 21:01:44 -0500

    Initial commit

---
+++ CalorieTracker/CalorieTracker/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CalorieTracker
+//
+//  Created by Josh Kocsis on 8/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
