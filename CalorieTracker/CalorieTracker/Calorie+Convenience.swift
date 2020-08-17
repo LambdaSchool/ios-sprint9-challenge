6526c0745eafcbafc764308603503f6714251cf4
Robert Vance <Fearmydotzs@gmail.com>
2020-08-16 21:34:44 -0600

    Almost Done with errors

---
+++ CalorieTracker/CalorieTracker/Calorie+Convenience.swift
@@ -0,0 +1,19 @@
+//
+//  Calorie+Convenience.swift
+//  CalorieTracker
+//
+//  Created by Rob Vance on 8/14/20.
+//  Copyright © 2020 Robs Creations. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Calorie {
+    convenience init(amount: Int, timestamp: Date = Date(),
+                     context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+           self.init(context: context)
+           self.amount = Int16(amount)
+           self.timestamp = timestamp
+       }
+}
---
+++ CalorieTracker/CalorieTracker/Model/Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.swift
+//  CalorieTracker
+//
+//  Created by Rob Vance on 8/14/20.
+//  Copyright © 2020 Robs Creations. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static var calorieLogChanged = Notification.Name("calorieLogChanged")
+}
