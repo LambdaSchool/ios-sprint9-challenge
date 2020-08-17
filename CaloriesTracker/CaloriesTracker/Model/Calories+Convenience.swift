cac375b082e54df84a4f769f1108dc319de316cd
Ian French <igf2011@hotmail.com>
2020-08-16 22:15:04 -0400

    Pod created for SwiftChart, and CoreData set up

---
+++ CaloriesTracker/CaloriesTracker/Model/Calories+Convenience.swift
@@ -0,0 +1,23 @@
+//
+//  Calories+Convenience.swift
+//  CaloriesTracker
+//
+//  Created by Ian French on 8/16/20.
+//  Copyright © 2020 Ian French. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Calorie {
+
+    @discardableResult convenience init(calorieValue: Double, date: Date, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.calorieValue = calorieValue
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy HH:mm:ss"
+        let formatDate = dateFormatter.string(from: Date())
+        self.date = formatDate
+    }
+}
---
+++ CaloriesTracker/CaloriesTracker/Model/CoreDataStack.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataStack.swift
+//  CaloriesTracker
+//
+//  Created by Ian French on 8/16/20.
+//  Copyright © 2020 Ian French. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    // MARK: - Properties
+    static let shared = CoreDataStack()
+
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Tasks")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistence stores: \(error)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+
+    // MARK: - Method
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch {
+                print("Error saving to CoreData \(error)")
+            }
+        }
+    }
+
+}
