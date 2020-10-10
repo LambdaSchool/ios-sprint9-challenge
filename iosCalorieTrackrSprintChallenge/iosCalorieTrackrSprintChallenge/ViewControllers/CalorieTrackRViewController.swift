4a3a119777bf59ff69d4d60c598c2a2e98b2a00d
BrysonSaclausa <brys1saclaus2@gmail.com>
2020-10-10 15:20:29 -0700

    text field alert,TableViewDelegate/datasource

--- iosCalorieTrackrSprintChallenge/iosCalorieTrackrSprintChallenge/ViewControllers/CalorieTrackRViewController.swift
+++ iosCalorieTrackrSprintChallenge/iosCalorieTrackrSprintChallenge/ViewControllers/CalorieTrackRViewController.swift
@@ -8,24 +8,53 @@
 import UIKit
 
 class CalorieTrackRViewController: UIViewController {
-
+    
+    //MARK: - IBoutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: - Properties
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 
     @IBAction func addTapped(_ sender: Any) {
+        showTextViewAlert()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     
+   @objc func showTextViewAlert() {
+        let alertView = UIAlertController(title: "Add Calorie Intake", message: "Enter The Amount OF Calories In The Field", preferredStyle: .alert)
+        alertView.addTextField(configurationHandler: nil)
+        alertView.textFields![0].placeholder = "Calories"
+        alertView.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alertView.addAction(UIAlertAction(title: "Done", style: .default, handler: { (_) in
+        print(alertView.textFields![0].text!)
+        }))
+        
+        self.present(alertView, animated: true, completion: nil)
+    
     }
-    */
+    
+    
 
+}
+
+extension CalorieTrackRViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+                
+                return cell!
+    }
+    
+    
 }
