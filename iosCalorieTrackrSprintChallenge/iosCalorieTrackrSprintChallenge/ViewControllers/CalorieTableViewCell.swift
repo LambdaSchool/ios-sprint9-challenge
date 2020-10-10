b4694d6ffec461532b83f787bdbc1288acdcb0a9
BrysonSaclausa <brys1saclaus2@gmail.com>
2020-10-10 12:02:04 -0700

    update: fix SB

---
+++ iosCalorieTrackrSprintChallenge/iosCalorieTrackrSprintChallenge/ViewControllers/CalorieTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CalorieTableViewCell.swift
+//  iosCalorieTrackrSprintChallenge
+//
+//  Created by BrysonSaclausa on 10/10/20.
+//
+
+import UIKit
+
+class CalorieTableViewCell: UITableViewCell {
+    
+    // MARK: - IBOUTLETS
+    
+    @IBOutlet weak var numberOfCaloriesLabel: UILabel!
+    @IBOutlet weak var timeStampLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iosCalorieTrackrSprintChallenge/iosCalorieTrackrSprintChallenge/ViewControllers/CalorieTrackRViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CalorieTrackRViewController.swift
+//  iosCalorieTrackrSprintChallenge
+//
+//  Created by BrysonSaclausa on 10/10/20.
+//
+
+import UIKit
+
+class CalorieTrackRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
