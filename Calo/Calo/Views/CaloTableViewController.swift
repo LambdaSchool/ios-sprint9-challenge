43b1826b7323953e0a859931e1247f05256699d2
Gladymir <gaethano815@gmail.com>
2020-10-11 10:12:38 -0600

    Created FRC, added FRC Delegate

--- Calo/Calo/Views/CaloTableViewController.swift
+++ Calo/Calo/Views/CaloTableViewController.swift
@@ -7,9 +7,36 @@
 //
 
 import UIKit
+import CoreData
+import SwiftChart
 
 class CaloTableViewController: UITableViewController {
+    
+    // Mark: IBOutlet
+    @IBOutlet private weak var chartView: Chart!
+    
+    // Mark: - Properties
+    let caloriesController = caloriesController()
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<CalorieEntry> = {
+        let fetchRequest: NSFetchRequest<CalorieEntry> = CalorieEntry.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date",
+        ascending: false)]
+        
+        let moc = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
+        
+        frc.delegate = self
+        
+        do {
+            try frc.performFetch()
+        } catch {
+            print("Error fetching")
+        }
+        return frc
+    }()
 
+    // Mark: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -35,50 +62,53 @@ class CaloTableViewController: UITableViewController {
         return cell
     }
     
+}
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+// Mark: Extensions
+
+extension CaloTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.begingUpdates()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                       didChange sectionInfo: NSFetchedResultsSectionInfo,
+                       atSectionIndex sectionIndex: Int,
+                       for type: NSFetchedResultsChangeType) {
+           switch type {
+           case .insert:
+               tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+           case .delete:
+               tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+           default:
+               break
+           }
+       }
+        
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            break
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
