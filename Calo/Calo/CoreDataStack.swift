786cf5dfaa1ccf51b8f0138af1b4fd26200224b7
Gladymir <gaethano815@gmail.com>
2020-10-11 09:44:02 -0600

    Created CoreData and Set up FileHierarchy

---
+++ Calo/Calo/CoreDataStack.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataStack.swift
+//  Calo
+//
+//  Created by Gladymir Philippe on 10/11/20.
+//  Copyright © 2020 Gladymir Philippe. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    //Stactic makes it a class property
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Entry")
+        container.loadPersistentStores { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistance stores: \(error)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var error: Error?
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+}
---
+++ Calo/Calo/Entry+Convenience.swift
@@ -0,0 +1,27 @@
+//
+//  Entry+Convenience.swift
+//  Calo
+//
+//  Created by Gladymir Philippe on 10/11/20.
+//  Copyright © 2020 Gladymir Philippe. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension CalorieEntry {
+    @discardableResult convenience init(calorieAmount: String, date: Date = Date(), context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.calorieAmount = calorieAmount
+        self.date = date
+    }
+    
+    var dateAdded: String {
+        guard let dateAdded = self.date else { return "" }
+        
+        let df = DateFormatter()
+        df.dateFormat = "MMM d Y 'at' h:mm a"
+        let date = df.string(from: dateAdded)
+        return date
+    }
+}
