d40d0dd9d13b4740d4f6c804c8046bb39c4c5dbd
Ben James <bejjmail@gmail.com>
2020-10-11 19:50:50 -0700

    created main storyboard and installed swiftlint

---
+++ Calorie Tracker/Calorie Tracker/Calorie_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_TrackerApp.swift
+//  Calorie Tracker
+//
+//  Created by Dojo on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Calorie_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
