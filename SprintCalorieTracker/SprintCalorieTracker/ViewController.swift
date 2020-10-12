9950d0d51d8dd50258bfa8894450710aa49964be
Zachary Thacker <zachthac@aol.com>
2020-10-11 22:29:13 -0400

    Set up Storyboard

---
+++ SprintCalorieTracker/SprintCalorieTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SprintCalorieTracker
+//
+//  Created by Zachary Thacker on 10/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
